917b99057051c123aae89a37bceb1ef8735cea09
christophd <deppisch@consol.de>
2009-07-22 18:26:07 +0000

    Fixed problem with duplicate correlationIds

--- modules/citrus-samples/src/main/java/com/consol/citrus/samples/flightbooking/FlightCompletionStrategy.java
+++ modules/citrus-samples/src/main/java/com/consol/citrus/samples/flightbooking/FlightCompletionStrategy.java
@@ -2,12 +2,20 @@ package com.consol.citrus.samples.flightbooking;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.consol.citrus.samples.flightbooking.model.FlightBookingConfirmationMessage;
 
 public class FlightCompletionStrategy {
     
+    Logger log = LoggerFactory.getLogger(FlightCompletionStrategy.class);
+    
     public boolean isComplete(List<FlightBookingConfirmationMessage> messages) {
-        return messages.size() % 2 == 0;
+        log.info("FlightAggregator (" + messages.get(0).getCorrelationId()
+                + ") complete = " + (messages.size() == 2));
+        
+        return messages.size() == 2;
     }
 
 }
