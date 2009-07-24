74b6dc666d8f9bd1c39d46cdda2c69469e532552
christophd <deppisch@consol.de>
2009-07-24 08:05:06 +0000

    Starting embedded activemq server in demo

--- modules/citrus-samples/src/main/java/com/consol/citrus/samples/flightbooking/FlightBookingDemo.java
+++ modules/citrus-samples/src/main/java/com/consol/citrus/samples/flightbooking/FlightBookingDemo.java
@@ -2,9 +2,14 @@ package com.consol.citrus.samples.flightbooking;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.consol.citrus.activemq.ActiveMQServer;
+
 public class FlightBookingDemo {
     
     public static void main(String[] args) {
+        ActiveMQServer activemqServer = new ActiveMQServer();
+        activemqServer.startup();
+        
         new ClassPathXmlApplicationContext("flightBooking.xml", FlightBookingDemo.class);
     }
 }
