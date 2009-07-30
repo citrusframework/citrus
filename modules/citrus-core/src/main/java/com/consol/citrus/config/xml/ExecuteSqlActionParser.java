3979bf276e2b02b637eccf42122da36b38994ee8
christophd <deppisch@consol.de>
2009-07-30 06:28:34 +0000

    Moved existing spring xml configruation parsers and namespace handler to new config package

---
+++ modules/citrus-core/src/main/java/com/consol/citrus/config/xml/ExecuteSqlActionParser.java
@@ -0,0 +1,61 @@
+package com.consol.citrus.config.xml;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.BeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.util.xml.DomUtils;
+import org.w3c.dom.Element;
+
+import com.consol.citrus.actions.ExecuteSqlBean;
+
+public class ExecuteSqlActionParser implements BeanDefinitionParser {
+
+    public BeanDefinition parse(Element element, ParserContext parserContext) {
+        String parentBeanName = element.getAttribute("connect");
+        BeanDefinitionBuilder beanDefinition;
+
+        if (parentBeanName != null && parentBeanName.length()>0) {
+            beanDefinition = BeanDefinitionBuilder.childBeanDefinition(parentBeanName);
+            beanDefinition.addPropertyValue("name", element.getLocalName() + ":" + parentBeanName);
+        } else {
+            beanDefinition = BeanDefinitionBuilder.rootBeanDefinition(ExecuteSqlBean.class);
+            beanDefinition.addPropertyValue("name", element.getLocalName());
+        }
+
+        DescriptionElementParser.doParse(element, beanDefinition);
+
+        List statements = new ArrayList();
+        List stmtElements = DomUtils.getChildElementsByTagName(element, "statement");
+        for (Iterator iter = stmtElements.iterator(); iter.hasNext();) {
+            Element stmt = (Element) iter.next();
+            statements.add(DomUtils.getTextValue(stmt));
+        }
+        beanDefinition.addPropertyValue("statements", statements);
+
+        Element sqlResourceElement = DomUtils.getChildElementByTagName(element, "resource");
+        if (sqlResourceElement != null) {
+            String filePath = sqlResourceElement.getAttribute("file");
+            if (filePath.startsWith("classpath:")) {
+                beanDefinition.addPropertyValue("sqlResource", new ClassPathResource(filePath.substring("classpath:".length())));
+            } else if (filePath.startsWith("file:")) {
+                beanDefinition.addPropertyValue("sqlResource", new FileSystemResource(filePath.substring("file:".length())));
+            } else {
+                beanDefinition.addPropertyValue("sqlResource", new FileSystemResource(filePath));
+            }
+        }
+
+        String ignoreErrors = element.getAttribute("ignoreErrors");
+        if (ignoreErrors != null && ignoreErrors.equals("true")) {
+            beanDefinition.addPropertyValue("ignoreErrors", true);
+        }
+
+        return beanDefinition.getBeanDefinition();
+    }
+}
---
+++ modules/citrus-core/src/main/java/com/consol/citrus/config/xml/SequenceParser.java
@@ -0,0 +1,50 @@
+package com.consol.citrus.config.xml;
+
+import java.util.Map;
+
+import org.apache.xerces.util.DOMUtil;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.ManagedList;
+import org.springframework.beans.factory.xml.BeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
+import com.consol.citrus.container.Sequence;
+
+public class SequenceParser implements BeanDefinitionParser {
+
+    public BeanDefinition parse(Element element, ParserContext parserContext) {
+        BeanDefinitionBuilder beanDefinition;
+
+        beanDefinition = BeanDefinitionBuilder.rootBeanDefinition(Sequence.class);
+
+        DescriptionElementParser.doParse(element, beanDefinition);
+
+        Map actionRegistry = TestActionRegistry.getRegisteredActionParser();
+        ManagedList actions = new ManagedList();
+
+        Element action = DOMUtil.getFirstChildElement(element);
+
+        if (action != null && action.getTagName().equals("description")) {
+            beanDefinition.addPropertyValue("description", action.getNodeValue());
+            action = DOMUtil.getNextSiblingElement(action);
+        }
+
+        if (action != null) {
+            do {
+                BeanDefinitionParser parser = (BeanDefinitionParser)actionRegistry.get(action.getTagName());
+
+                actions.add(parser.parse(action, parserContext));
+            } while ((action = DOMUtil.getNextSiblingElement(action)) != null);
+        }
+
+        if (actions.size() > 0) {
+            beanDefinition.addPropertyValue("actions", actions);
+        }
+
+        beanDefinition.addPropertyValue("name", element.getLocalName());
+
+        return beanDefinition.getBeanDefinition();
+    }
+}
