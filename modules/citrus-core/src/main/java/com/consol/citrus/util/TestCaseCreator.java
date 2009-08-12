c3cdb6e76486ea7e70ff3d9ac93e8221347354f8
christophd <deppisch@consol.de>
2009-08-12 07:45:43 +0000

    TestCaseCreator always goes with Java generated unit test now

--- modules/citrus-core/src/main/java/com/consol/citrus/util/TestCaseCreator.java
+++ modules/citrus-core/src/main/java/com/consol/citrus/util/TestCaseCreator.java
@@ -5,10 +5,8 @@ import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 import java.util.Properties;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.consol.citrus.TestConstants;
+import com.consol.citrus.exceptions.CitrusRuntimeException;
 
 /**
  * CLI creating a new test case from template.
@@ -23,51 +21,55 @@ public class TestCaseCreator {
     
     private String description;
     
-    private String directory;
-    
-    /**
-     * Logger
-     */
-    private static final Logger log = LoggerFactory.getLogger(TestCaseCreator.class);
+    private String targetPackage;
     
     public static void main(String[] args) {
         try {    
-            TestCaseCreator creator = new TestCaseCreator();
-            
-            creator.setName(args[0]);
-            creator.setAuthor(args[1]);
-            creator.setDescription(args[2]);
-            creator.setDirectory(args[3]);
+            TestCaseCreator creator = TestCaseCreator.build()
+                .withName(args[0])
+                .withAuthor(args[1])
+                .withDescription(args[2])
+                .usePackage(args[3]);
             
             creator.createTestCase();
         } catch (ArrayIndexOutOfBoundsException e) {
-            log.info("Wrong usage exception!");
-            log.info("Use parameters in the following way: [test.name] [test.author] [test.description] [test.subfolder]");
+            throw new CitrusRuntimeException("Wrong usage exception! " +
+            		"Use parameters in the following way: [test.name] [test.author] [test.description] [test.subfolder]", e);
         }
     }
     
     public void createTestCase() {
+        Properties properties = new Properties();
+        properties.put("test.name", name);
+        properties.put("test.author", author);
+        properties.put("test.description", description);
+        
+        properties.put("test.updatedon.datetime", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(GregorianCalendar.getInstance().getTime()));
+        properties.put("test.creation.date", new SimpleDateFormat("yyyy-MM-dd").format(GregorianCalendar.getInstance().getTime()));
+        
+        properties.put("test.method.name", name.substring(0,1).toLowerCase() + name.substring(1));
+        properties.put("test.package", targetPackage);
+        
+        targetPackage = targetPackage.replace('.', '/');
+        
+        createXMLFile(properties);    
+        createJavaFile(properties);
+    }
+    
+    private void createJavaFile(Properties properties) {
         BufferedReader reader = null;
         OutputStream buffered = null;
         
         try {
-            Properties properties = new Properties();
-            properties.put("test.name", name);
-            properties.put("test.author", author);
-            properties.put("test.description", description);
-            
-            properties.put("test.updatedon.datetime", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(GregorianCalendar.getInstance().getTime()));
-            properties.put("test.creation.date", new SimpleDateFormat("yyyy-MM-dd").format(GregorianCalendar.getInstance().getTime()));
-            
-            File file = new File(directory + "/" + name + "." + TestConstants.TESTCASE_FILE_EXTENSION);
+            File file = new File("src/citrus/java/" + targetPackage + "/" + name + ".java");
             if(file.getParentFile().exists() == false) {
                 file.getParentFile().mkdirs();
             }
                 
             FileOutputStream fos = new FileOutputStream(file);
             buffered = new BufferedOutputStream(fos);
-
-            reader = new BufferedReader(new InputStreamReader(TestCaseCreator.class.getResourceAsStream("test-template.xml")));
+    
+            reader = new BufferedReader(new InputStreamReader(TestCaseCreator.class.getResourceAsStream("java-template.txt")));
             
             StringWriter sWriter = new StringWriter();
             
@@ -75,13 +77,11 @@ public class TestCaseCreator {
             while ((line = reader.readLine()) != null) {
                 sWriter.append(PropertyUtils.replacePropertiesInString(line, properties) + "\n");
             }
-
+    
             buffered.write(sWriter.toString().getBytes());
             buffered.flush();
             fos.flush();
             fos.close();
-            
-            log.info("Successfully created new test case " + directory + "/" + name);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -99,6 +99,75 @@ public class TestCaseCreator {
                 e.printStackTrace();
             }
         }
+    }
+    
+    private void createXMLFile(Properties properties) {
+        BufferedReader reader = null;
+        OutputStream buffered = null;
+        
+        try {
+            File file = new File("src/citrus/resources/" + targetPackage + "/" + name + "." + TestConstants.TESTCASE_FILE_EXTENSION);
+            if(file.getParentFile().exists() == false) {
+                file.getParentFile().mkdirs();
+            }
+                
+            FileOutputStream fos = new FileOutputStream(file);
+            buffered = new BufferedOutputStream(fos);
+    
+            reader = new BufferedReader(new InputStreamReader(TestCaseCreator.class.getResourceAsStream("test-template.xml")));
+            
+            StringWriter sWriter = new StringWriter();
+            
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sWriter.append(PropertyUtils.replacePropertiesInString(line, properties) + "\n");
+            }
+    
+            buffered.write(sWriter.toString().getBytes());
+            buffered.flush();
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(buffered != null) {
+                    buffered.close();
+                }
+                
+                if(reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static TestCaseCreator build() {
+        return new TestCaseCreator();
+    }
+    
+    public TestCaseCreator withName(String name) {
+        this.name = name;
+        return this;
+    }
+    
+    public TestCaseCreator withAuthor(String author) {
+        this.author = author;
+        return this;
+    }
+    
+    public TestCaseCreator withDescription(String description) {
+        this.description = description;
+        return this;
+    }
+    
+    public TestCaseCreator usePackage(String targetPackage) {
+        this.targetPackage = targetPackage;
+        return this;
     }
 
     /**
@@ -144,16 +213,16 @@ public class TestCaseCreator {
     }
 
     /**
-     * @param directory the directory to set
+     * @param targetPackage the targetPackage to set
      */
-    public void setDirectory(String directory) {
-        this.directory = directory;
+    public void setPackage(String targetPackage) {
+        this.targetPackage = targetPackage;
     }
 
     /**
-     * @return the directory
+     * @return the targetPackage
      */
-    public String getDirectory() {
-        return directory;
+    public String getPackage() {
+        return targetPackage;
     }
 }
