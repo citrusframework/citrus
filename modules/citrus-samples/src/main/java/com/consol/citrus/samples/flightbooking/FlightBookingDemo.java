e3a62f516dd3c38be318a59c9e93007229124690
christophd <deppisch@consol.de>
2009-08-14 08:15:20 +0000

    Added autostart to ActiveMQServer and cleaned up application context namespaces

--- modules/citrus-samples/src/main/java/com/consol/citrus/samples/flightbooking/FlightBookingDemo.java
+++ modules/citrus-samples/src/main/java/com/consol/citrus/samples/flightbooking/FlightBookingDemo.java
@@ -2,14 +2,9 @@ package com.consol.citrus.samples.flightbooking;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.consol.citrus.activemq.ActiveMQServer;
-
 public class FlightBookingDemo {
     
     public static void main(String[] args) {
-        ActiveMQServer activemqServer = new ActiveMQServer();
-        activemqServer.start();
-        
         new ClassPathXmlApplicationContext("flightBooking.xml", FlightBookingDemo.class);
     }
 }
