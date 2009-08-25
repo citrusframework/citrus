186ba7a39435db75151f5132c22d32efd3eecd2e
christophd <deppisch@consol.de>
2009-08-25 20:59:49 +0000

    Fixed samples in assembly

--- modules/citrus-samples/src/main/script/flightBookingDemo.sh
+++ modules/citrus-samples/src/main/script/flightBookingDemo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 JARS=`find lib ../dist ../lib -iname *.jar`
 CP=`echo $JARS | sed 's/ /:/g'`
-java -cp $CP com.consol.citrus.samples.flightbooking.FlightBookingDemo $*
+java -cp $CP:src/citrus/resources com.consol.citrus.samples.flightbooking.FlightBookingDemo $*
