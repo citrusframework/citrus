00c3c25c294a5fe626de25d5c4e3cc14f0c303dd
christophd <deppisch@consol.de>
2009-09-03 08:16:56 +0000

    Refactored purge jms queues bean

--- modules/citrus-core/src/main/java/com/consol/citrus/actions/PurgeJmsQueuesBean.java
+++ modules/citrus-core/src/main/java/com/consol/citrus/actions/PurgeJmsQueuesBean.java
@@ -1,19 +1,28 @@
 package com.consol.citrus.actions;
 
-import java.security.AccessControlException;
-import java.util.Iterator;
 import java.util.List;
 
-import javax.jms.*;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.Session;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.connection.ConnectionFactoryUtils;
+import org.springframework.jms.support.JmsUtils;
+import org.springframework.jms.support.destination.DynamicDestinationResolver;
 
 import com.consol.citrus.context.TestContext;
 import com.consol.citrus.exceptions.CitrusRuntimeException;
 
 /**
- * Bean to explicitly purge jms queues.
+ * Bean to purge JMS queue destinations by simply consuming all available messages.
  *
  * @author deppisch Christoph Deppisch ConSol* Software GmbH
  * @since 24.01.2007
@@ -21,13 +30,14 @@ import com.consol.citrus.exceptions.CitrusRuntimeException;
 public class PurgeJmsQueuesBean extends AbstractTestAction {
 
     /** List of queues to be purged */
-    private List queueNames;
+    private List<String> queueNames;
 
-    /** QueueConnectionFactory */
-    private QueueConnectionFactory connectionFactory;
+    @Autowired
+    /** ConnectionFactory */
+    private ConnectionFactory connectionFactory;
 
     /** Time to wait until timeout in ms */
-    private long receiveTimeout = 10;
+    private long receiveTimeout = 100;
 
     /**
      * Logger
@@ -42,86 +52,94 @@ public class PurgeJmsQueuesBean extends AbstractTestAction {
     public void execute(TestContext context) {
         log.info("Purging JMS queues...");
 
-        QueueConnection qcon = null;
-        QueueSession qsession = null;
-        QueueReceiver qreceiver = null;
-
+        Connection connection = null;
+        Session session = null;
+        MessageConsumer messageConsumer = null;
+        
         try {
-            qcon = connectionFactory.createQueueConnection();
-            qsession = qcon.createQueueSession(false, javax.jms.Session.AUTO_ACKNOWLEDGE);
-
-            for (Iterator iter = queueNames.iterator(); iter.hasNext();) {
-                String queueName = (String) iter.next();
-
+        	connection = createConnection();
+            session = createSession(connection);
+            connection.start();
+            
+            for (String queueName : queueNames) {
                 if (log.isDebugEnabled()) {
                     log.debug("Try to purge queue " + queueName);
                 }
 
-                Queue queue = qsession.createQueue(queueName);
-                qreceiver = qsession.createReceiver(queue);
-
-                qcon.start();
-                while (qreceiver.receive(receiveTimeout) != null) {
-                    if (log.isDebugEnabled()) {
+                messageConsumer = session.createConsumer(getDestination(session, queueName));
+                
+                javax.jms.Message message;
+                do {
+                	message = (receiveTimeout >= 0) ? messageConsumer.receive(receiveTimeout) : messageConsumer.receive();
+                	
+                    if (message != null && log.isDebugEnabled()) {
                         log.debug("Removed message from queue " + queueName);
                     }
-                }
+                } while (message != null);
 
-                qreceiver.close();
+                JmsUtils.closeMessageConsumer(messageConsumer);
             }
         } catch (JMSException e) {
             log.error("Error while establishing jms queue connection", e);
             throw new CitrusRuntimeException(e);
         } finally {
-            if (qreceiver != null) {
-                try {
-                    qreceiver.close();
-                } catch (JMSException e) {
-                    log.error("Error while closing the jms queue receiver", e);
-                }
-            }
+        	JmsUtils.closeMessageConsumer(messageConsumer);
+            JmsUtils.closeSession(session);
 
-            if (qsession != null) {
-                try {
-                    qsession.close();
-                } catch (JMSException e) {
-                    log.error("Error while closing the jms queue session", e);
-                }
-            }
-
-            if (qcon != null) {
-                try {
-                    qcon.close();
-                } catch (JMSException e) {
-                    log.error("Error while closing the jms queue connection", e);
-                } catch (AccessControlException e) {
-                    log.debug("Error while closing the jms queue connection", e);
-                }
+            if(connection != null) {
+                ConnectionFactoryUtils.releaseConnection(connection, this.connectionFactory, true);
             }
         }
 
         log.info("JMS queues purged successfully");
     }
+    
+    private Destination getDestination(Session session, String queueName) throws JMSException {
+    	return new DynamicDestinationResolver().resolveDestinationName(session, queueName, false);
+	}
+
+	/**
+     * @return
+     * @throws JMSException
+     */
+    protected Connection createConnection() throws JMSException {
+        if (connectionFactory instanceof QueueConnectionFactory) {
+            return ((QueueConnectionFactory) connectionFactory).createQueueConnection();
+        }
+        return connectionFactory.createConnection();
+    }
+    
+    /**
+     * 
+     * @param connection
+     * @return
+     * @throws JMSException
+     */
+    protected Session createSession(Connection connection) throws JMSException {
+        if (connection instanceof QueueConnection) {
+            return ((QueueConnection) connection).createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+        }
+        return connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    }
 
     /**
      * @param queueNames the queueNames to set
      */
-    public void setQueueNames(List queueNames) {
+    public void setQueueNames(List<String> queueNames) {
         this.queueNames = queueNames;
     }
 
     /**
      * @param queueConnectionFactory the queueConnectionFactory to set
      */
-    public void setQueueConnectionFactory(
-            QueueConnectionFactory queueConnectionFactory) {
-        this.connectionFactory = queueConnectionFactory;
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
     }
 
     /**
      * @return the queueNames
      */
-    public List getQueueNames() {
+    public List<String> getQueueNames() {
         return queueNames;
     }
 }
