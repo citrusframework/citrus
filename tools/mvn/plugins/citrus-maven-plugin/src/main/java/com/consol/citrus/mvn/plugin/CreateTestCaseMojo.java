1c8e248e444e9e9c14388d5d36c1dd96bd8871c3
christophd <deppisch@consol.de>
2009-10-06 14:32:46 +0000

    Added interactive mode prompting user for information

--- tools/mvn/plugins/citrus-maven-plugin/src/main/java/com/consol/citrus/mvn/plugin/CreateTestCaseMojo.java
+++ tools/mvn/plugins/citrus-maven-plugin/src/main/java/com/consol/citrus/mvn/plugin/CreateTestCaseMojo.java
@@ -21,7 +21,12 @@ package com.consol.citrus.mvn.plugin;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.components.interactivity.Prompter;
+import org.codehaus.plexus.components.interactivity.PrompterException;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
+import com.consol.citrus.exceptions.CitrusRuntimeException;
 import com.consol.citrus.util.TestCaseCreator;
 
 /**
@@ -30,8 +35,9 @@ import com.consol.citrus.util.TestCaseCreator;
  * @goal create-test
  */
 public class CreateTestCaseMojo extends AbstractMojo {
-    /** @parameter expression="${name}"
-     *  @required */
+    /** @parameter 
+     *          expression="${name}" 
+     *          default-value="" */
     private String name;
     
     /** @parameter
@@ -49,8 +55,41 @@ public class CreateTestCaseMojo extends AbstractMojo {
      *          default-value="com.consol.citrus" */
     private String targetPackage;
     
+    /** @parameter 
+     *          expression="${interactiveMode}"
+     *          default-value="true" */
+    private boolean interactiveMode;
+    
+    /** @component
+     *  @required */
+    private Prompter prompter;
+    
     public void execute() throws MojoExecutionException {
-        try {    
+        try {
+        	while(interactiveMode && StringUtils.hasText(name) == false) {
+        		name = prompter.prompt("Enter test name");
+        	}
+        	
+        	if(StringUtils.hasText(name) == false) {
+        		throw new CitrusRuntimeException("Test must have a name!");
+        	}
+        	
+        	if(interactiveMode) {
+        		author = prompter.prompt("Enter test author:", author);
+        		description = prompter.prompt("Enter test description:", description);
+        		targetPackage = prompter.prompt("Enter test package:", targetPackage);
+        		
+        		String confirm = prompter.prompt("Confirm test creation:\n" +
+    			        "name: " + name + "\n" +
+    					"author: " + author + "\n" +
+    					"description: " + description + "\n" +
+    					"package: " + targetPackage + "\n", CollectionUtils.arrayToList(new String[] {"y", "n"}), "y");
+    	
+		    	if(confirm.equalsIgnoreCase("n")) {
+		    		return;
+		    	}
+        	}
+        	
             TestCaseCreator creator = TestCaseCreator.build()
                 .withName(name)
                 .withAuthor(author)
@@ -58,9 +97,17 @@ public class CreateTestCaseMojo extends AbstractMojo {
                 .usePackage(targetPackage);
             
             creator.createTestCase();
+            
+            getLog().info("Successfully created new test case \n" +
+            		    "name: " + name + "\n" +
+    					"author: " + author + "\n" +
+    					"description: " + description + "\n" +
+    					"package: " + targetPackage);
         } catch (ArrayIndexOutOfBoundsException e) {
             getLog().info("Wrong usage exception!");
             getLog().info("Use parameters in the following way: [test.name] [test.author] [test.description] [test.package]");
-        }
+        } catch (PrompterException e) {
+			getLog().info(e);
+		}
     }
 }
