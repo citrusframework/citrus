d6f6aff7a9c1a79d353c2239dcadc07a5a406b70
christophd <deppisch@consol.de>
2009-11-17 21:08:38 +0000

    Added some unit tests

---
+++ modules/citrus-core/src/test/java/com/consol/citrus/group/IterateTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2006-2009 ConSol* Software GmbH.
+ * 
+ * This file is part of Citrus.
+ * 
+ *  Citrus is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Citrus is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Citrus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.consol.citrus.group;
+
+import static org.easymock.EasyMock.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.easymock.EasyMock;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.consol.citrus.AbstractBaseTest;
+import com.consol.citrus.TestAction;
+
+/**
+ * @author Christoph Christoph Deppisch Consol* Software GmbH
+ */
+public class IterateTest extends AbstractBaseTest {
+    @Test
+    public void testIteration() {
+        Iterate iterate = new Iterate();
+        
+        List<TestAction> actions = new ArrayList<TestAction>();
+        TestAction action = EasyMock.createMock(TestAction.class);
+
+        reset(action);
+        
+        action.execute(context);
+        expectLastCall().times(5);
+        
+        replay(action);
+        
+        actions.add(action);
+        iterate.setActions(actions);
+        
+        iterate.setCondition("i lt= 5");
+        iterate.setIndexName("i");
+        
+        iterate.execute(context);
+        
+        Assert.assertNotNull(context.getVariable("${i}"));
+        Assert.assertEquals(context.getVariable("${i}"), "5");
+    }
+    
+    @Test
+    public void testStep() {
+        Iterate iterate = new Iterate();
+        
+        List<TestAction> actions = new ArrayList<TestAction>();
+        TestAction action = EasyMock.createMock(TestAction.class);
+
+        reset(action);
+        
+        action.execute(context);
+        expectLastCall().times(5);
+        
+        replay(action);
+        
+        actions.add(action);
+        iterate.setActions(actions);
+        
+        iterate.setCondition("i lt= 10");
+        iterate.setIndexName("i");
+        iterate.setStep(2);
+        
+        iterate.execute(context);
+        
+        Assert.assertNotNull(context.getVariable("${i}"));
+        Assert.assertEquals(context.getVariable("${i}"), "9");
+    }
+    
+    @Test
+    public void testNoIterationBasedOnCondition() {
+        Iterate iterate = new Iterate();
+        
+        List<TestAction> actions = new ArrayList<TestAction>();
+        TestAction action = EasyMock.createMock(TestAction.class);
+
+        reset(action);
+        replay(action);
+        
+        actions.add(action);
+        iterate.setActions(actions);
+        
+        iterate.setCondition("i lt 0");
+        iterate.setIndexName("i");
+        
+        iterate.execute(context);
+        
+        Assert.assertNull(context.getVariables().get("i"));
+    }
+}
---
+++ modules/citrus-core/src/test/java/com/consol/citrus/group/RepeatUntilTrueTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2006-2009 ConSol* Software GmbH.
+ * 
+ * This file is part of Citrus.
+ * 
+ *  Citrus is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Citrus is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Citrus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.consol.citrus.group;
+
+import static org.easymock.EasyMock.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.easymock.EasyMock;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.consol.citrus.AbstractBaseTest;
+import com.consol.citrus.TestAction;
+
+/**
+ * @author Christoph Christoph Deppisch Consol* Software GmbH
+ */
+public class RepeatUntilTrueTest extends AbstractBaseTest {
+    @Test
+    public void testRepeat() {
+        RepeatUntilTrue repeatUntilTrue = new RepeatUntilTrue();
+        
+        List<TestAction> actions = new ArrayList<TestAction>();
+        TestAction action = EasyMock.createMock(TestAction.class);
+
+        reset(action);
+        
+        action.execute(context);
+        expectLastCall().times(4);
+        
+        replay(action);
+        
+        actions.add(action);
+        repeatUntilTrue.setActions(actions);
+        
+        repeatUntilTrue.setCondition("i = 5");
+        repeatUntilTrue.setIndexName("i");
+        
+        repeatUntilTrue.execute(context);
+        
+        Assert.assertNotNull(context.getVariable("${i}"));
+        Assert.assertEquals(context.getVariable("${i}"), "4");
+    }
+    
+    @Test
+    public void testRepeatMinimumOnce() {
+        RepeatUntilTrue repeatUntilTrue = new RepeatUntilTrue();
+        
+        List<TestAction> actions = new ArrayList<TestAction>();
+        TestAction action = EasyMock.createMock(TestAction.class);
+
+        reset(action);
+        
+        action.execute(context);
+        expectLastCall().once();
+        
+        replay(action);
+        
+        actions.add(action);
+        repeatUntilTrue.setActions(actions);
+        
+        repeatUntilTrue.setCondition("i gt 0");
+        repeatUntilTrue.setIndexName("i");
+        
+        repeatUntilTrue.execute(context);
+        
+        Assert.assertNotNull(context.getVariable("${i}"));
+        Assert.assertEquals(context.getVariable("${i}"), "1");
+    }
+}
