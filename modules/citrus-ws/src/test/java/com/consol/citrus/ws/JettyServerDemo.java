092aad9562e083a9b1be8396ffe89069b23b69ba
christophd <deppisch@consol.de>
2010-01-25 20:23:36 +0000

    Adding java doc comments

--- modules/citrus-ws/src/test/java/com/consol/citrus/ws/JettyServerDemo.java
+++ modules/citrus-ws/src/test/java/com/consol/citrus/ws/JettyServerDemo.java
@@ -21,8 +21,14 @@ package com.consol.citrus.ws;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * @author Christoph Deppisch
+ */
 public class JettyServerDemo {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         new ClassPathXmlApplicationContext("jetty-server-demo.xml", JettyServerDemo.class);
     }
