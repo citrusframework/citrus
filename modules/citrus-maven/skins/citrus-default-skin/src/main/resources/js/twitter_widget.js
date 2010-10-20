4145ceae20e9a08c4f8fb77050e96cc7a41e11f5
christophd <deppisch@consol.de>
2010-10-20 23:56:39 +0200

    Added twitter widget and news rss feed widget to maven generated site Improved css on maven generated site

---
+++ modules/citrus-maven/skins/citrus-default-skin/src/main/resources/js/twitter_widget.js
@@ -0,0 +1,28 @@
+new TWTR.Widget({
+	version: 2,
+	type: 'profile',
+	rpp: 3,
+	interval: 6000,
+	width: 250,
+	height: 300,
+	theme: {
+	  shell: {
+	    background: '#FAFAFA',
+	    color: '#000000'
+	  },
+	  tweets: {
+	    background: '#FFFFFF',
+	    color: '#000000',
+	    links: '##FF8805'
+	  }
+	},
+	features: {
+	  scrollbar: false,
+	  loop: false,
+	  live: false,
+	  hashtags: true,
+	  timestamp: true,
+	  avatars: false,
+	  behavior: 'all'
+	}
+}).render().setUser('citrus_test').start();
