5ff1185f352f822f44235e8144fa52b04bbd451e
christophd <deppisch@consol.de>
2011-01-20 16:12:20 +0100

    Working on Maven generated site

--- src/site/resources/js/newsarchive.js
+++ src/site/resources/js/newsarchive.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     $('#newsfeed').rssfeed('http://labs.consol.de/tags/citrus/feed/', {
       header: false,
-      snippet: false,
-      limit: 5
+      snippet: false
     });
 });
