595aef58ef511d3cddbe824cb9acdda434c9a5a0
christophd <deppisch@consol.de>
2011-07-21 17:29:24 +0200

    Improved method complexity in this bean definition parser

--- modules/citrus-core/src/main/java/com/consol/citrus/config/xml/TestCaseParser.java
+++ modules/citrus-core/src/main/java/com/consol/citrus/config/xml/TestCaseParser.java
@@ -20,18 +20,17 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-import org.apache.xerces.util.DOMUtil;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.ManagedList;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.util.StringUtils;
 import org.springframework.util.xml.DomUtils;
 import org.w3c.dom.Element;
 
-import com.consol.citrus.TestCase;
-import com.consol.citrus.TestCaseMetaInfo;
+import com.consol.citrus.*;
 import com.consol.citrus.TestCaseMetaInfo.Status;
 import com.consol.citrus.config.TestActionRegistry;
 import com.consol.citrus.config.TestCaseFactory;
@@ -44,22 +43,97 @@ import com.consol.citrus.variable.VariableUtils;
  */
 public class TestCaseParser implements BeanDefinitionParser {
 
-    /** Name of the test */
-    private String testCaseName = "";
-
     /**
      * (non-Javadoc)
      * @see org.springframework.beans.factory.xml.BeanDefinitionParser#parse(org.w3c.dom.Element, org.springframework.beans.factory.xml.ParserContext)
      */
-    @SuppressWarnings({ "unchecked", "rawtypes" })
 	public final BeanDefinition parse(Element element, ParserContext parserContext) {
         BeanDefinitionBuilder testCaseFactory = BeanDefinitionBuilder.rootBeanDefinition(TestCaseFactory.class);
         BeanDefinitionBuilder testcase = BeanDefinitionBuilder.rootBeanDefinition(TestCase.class);
 
-        testCaseName = element.getAttribute("name");
+        String testName = element.getAttribute("name");
+        if (!StringUtils.hasText(testName)) {
+            throw new BeanCreationException("Please provide proper test case name");
+        }
+        
+        testcase.addPropertyValue("name", testName);
 
-        testcase.addPropertyValue("name", testCaseName);
+        parseMetaInfo(testcase, element);
+        parseVariableDefinitions(testcase, element);
 
+        DescriptionElementParser.doParse(element, testcase);
+        
+        Element actionsElement = DomUtils.getChildElementByTagName(element, "actions");
+        Element finallyBlockElement = DomUtils.getChildElementByTagName(element, "finally");
+
+        testCaseFactory.addPropertyValue("testCase", testcase.getBeanDefinition());
+        testCaseFactory.addPropertyValue("testChain", parseActions(actionsElement, parserContext, TestActionRegistry.getRegisteredActionParser()));
+        testCaseFactory.addPropertyValue("finallyChain", parseActions(finallyBlockElement, parserContext, TestActionRegistry.getRegisteredActionParser()));
+
+        parserContext.getRegistry().registerBeanDefinition(testName, testCaseFactory.getBeanDefinition());
+
+        return parserContext.getRegistry().getBeanDefinition(testName);
+    }
+
+    /**
+     * Parses action elements and adds them to a managed list.
+     * @param actionsContainerElement the action container.
+     * @param parserContext the current parser context.
+     * @return
+     */
+    private ManagedList<BeanDefinition> parseActions(Element actionsContainerElement, ParserContext parserContext, 
+            Map<String, BeanDefinitionParser> actionRegistry) {
+        ManagedList<BeanDefinition> actions = new ManagedList<BeanDefinition>();
+        
+        if (actionsContainerElement != null) {
+            List<Element> actionList = DomUtils.getChildElements(actionsContainerElement);
+            for (Element action : actionList) {
+                BeanDefinitionParser parser = (BeanDefinitionParser)actionRegistry.get(action.getTagName());
+                
+                if (parser ==  null) {
+                    actions.add(parserContext.getReaderContext().getNamespaceHandlerResolver().resolve(action.getNamespaceURI()).parse(action, parserContext));
+                } else {
+                    actions.add(parser.parse(action, parserContext));
+                }
+            }
+        }
+        
+        return actions;
+    }
+
+    /**
+     * Parses all variable definitions and adds those to the bean definition 
+     * builder for this test case.
+     * @param testcase the target bean definition builder for this test case.
+     * @param element the source element.
+     */
+    private void parseVariableDefinitions(BeanDefinitionBuilder testcase, Element element) {
+        Element variablesElement = DomUtils.getChildElementByTagName(element, "variables");
+        if (variablesElement != null) {
+            Map<String, String> testVariables = new LinkedHashMap<String, String>();
+            List<?> variableElements = DomUtils.getChildElementsByTagName(variablesElement, "variable");
+            for (Iterator<?> iter = variableElements.iterator(); iter.hasNext();) {
+                Element variableDefinition = (Element) iter.next();
+                Element variableValueElement = DomUtils.getChildElementByTagName(variableDefinition, "value");
+                if (variableValueElement == null) {
+                    testVariables.put(variableDefinition.getAttribute("name"), variableDefinition.getAttribute("value"));
+                } else {
+                    Element variableScript = DomUtils.getChildElementByTagName(variableValueElement, "script");
+                    String scriptEngine = variableScript.getAttribute("type");
+                    testVariables.put(variableDefinition.getAttribute("name"), VariableUtils.getValueFromScript(scriptEngine, 
+                            variableScript.getTextContent()));
+                }
+            }
+            testcase.addPropertyValue("variableDefinitions", testVariables);
+        }
+    }
+
+    /**
+     * Parses meta information and adds it to the test case bean definition builder.
+     * @param testcase the target bean definition builder for this test case.
+     * @param element the source element.
+     */
+    private void parseMetaInfo(BeanDefinitionBuilder testcase, Element element) {
         Element metaInfoElement = DomUtils.getChildElementByTagName(element, "meta-info");
         if (metaInfoElement != null) {
             TestCaseMetaInfo metaInfo = new TestCaseMetaInfo();
@@ -102,79 +176,5 @@ public class TestCaseParser implements BeanDefinitionParser {
 
             testcase.addPropertyValue("metaInfo", metaInfo);
         }
-
-        DescriptionElementParser.doParse(element, testcase);
-
-        Element testVariablesElement = DomUtils.getChildElementByTagName(element, "variables");
-
-        if (testVariablesElement != null) {
-            Map<String, String> testVariables = new LinkedHashMap<String, String>();
-            List<?> variableElements = DomUtils.getChildElementsByTagName(testVariablesElement, "variable");
-            for (Iterator<?> iter = variableElements.iterator(); iter.hasNext();) {
-                Element variableDefinition = (Element) iter.next();
-                Element variableValueElement = DomUtils.getChildElementByTagName(variableDefinition, "value");
-                if (variableValueElement == null) {
-                	testVariables.put(variableDefinition.getAttribute("name"), variableDefinition.getAttribute("value"));
-                } else {
-                	Element variableScript = DomUtils.getChildElementByTagName(variableValueElement, "script");
-                	String scriptEngine = variableScript.getAttribute("type");
-                	testVariables.put(variableDefinition.getAttribute("name"), VariableUtils.getValueFromScript(scriptEngine, 
-                	        variableScript.getTextContent()));
-                }
-            }
-            testcase.addPropertyValue("variableDefinitions", testVariables);
-        }
-
-        Map<String, BeanDefinitionParser> actionRegistry = TestActionRegistry.getRegisteredActionParser();
-
-        Element testChainElement = DomUtils.getChildElementByTagName(element, "actions");
-        ManagedList testChain = new ManagedList();
-
-        Element action = DOMUtil.getFirstChildElement(testChainElement);
-        if (action != null) {
-            do {
-                BeanDefinitionParser parser = (BeanDefinitionParser)actionRegistry.get(action.getTagName());
-                
-                if(parser ==  null) {
-                	testChain.add(parserContext.getReaderContext().getNamespaceHandlerResolver().resolve(action.getNamespaceURI()).parse(action, parserContext));
-                } else {
-                	testChain.add(parser.parse(action, parserContext));
-                }
-                
-                action = DOMUtil.getNextSiblingElement(action);
-            } while (action != null);
-        }
-
-        Element finallyChainElement = DomUtils.getChildElementByTagName(element, "finally");
-        ManagedList finallyChain = new ManagedList();
-
-        if (finallyChainElement != null) {
-            action = DOMUtil.getFirstChildElement(finallyChainElement);
-            if (action != null) {
-                do {
-                    BeanDefinitionParser parser = (BeanDefinitionParser)actionRegistry.get(action.getTagName());
-                    
-                    if(parser ==  null) {
-                    	finallyChain.add(parserContext.getReaderContext().getNamespaceHandlerResolver().resolve(action.getNamespaceURI()).parse(action, parserContext));
-                    } else {
-                    	finallyChain.add(parser.parse(action, parserContext));
-                    }
-                    
-                    action = DOMUtil.getNextSiblingElement(action);
-                } while (action != null);
-            }
-        }
-
-        testCaseFactory.addPropertyValue("testCase", testcase.getBeanDefinition());
-        testCaseFactory.addPropertyValue("testChain", testChain);
-        testCaseFactory.addPropertyValue("finallyChain", finallyChain);
-
-        if (testCaseName != null) {
-            parserContext.getRegistry().registerBeanDefinition(testCaseName, testCaseFactory.getBeanDefinition());
-        } else {
-            throw new RuntimeException("Test case name is missing");
-        }
-
-        return parserContext.getRegistry().getBeanDefinition(testCaseName);
     }
 }
