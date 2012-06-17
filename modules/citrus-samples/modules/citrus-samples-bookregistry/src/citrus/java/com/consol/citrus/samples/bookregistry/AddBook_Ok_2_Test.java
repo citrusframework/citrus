31f7496593a14306bdc0c9d0c0f1f3625876dbf5
christophd <deppisch@consol.de>
2012-06-17 15:28:44 +0200

    New test in book registry sample for Java DSL

---
+++ modules/citrus-samples/modules/citrus-samples-bookregistry/src/citrus/java/com/consol/citrus/samples/bookregistry/AddBook_Ok_2_Test.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2006-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.samples.bookregistry;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.integration.MessageHeaders;
+import org.springframework.oxm.castor.CastorMarshaller;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+import com.consol.citrus.dsl.CitrusTestNGTestCaseBuilder;
+import com.consol.citrus.message.MessageReceiver;
+import com.consol.citrus.message.MessageSender;
+import com.consol.citrus.samples.CitrusSamplesDemo;
+import com.consol.citrus.samples.bookregistry.model.*;
+import com.consol.citrus.validation.callback.AbstractValidationCallback;
+
+/**
+ *
+ * @author Christoph Deppisch
+ */
+public class AddBook_Ok_2_Test extends CitrusTestNGTestCaseBuilder {
+
+    private BookRegistryDemo demo = new BookRegistryDemo();
+    
+    @Autowired
+    @Qualifier("bookRegistryRequestMessageSender")
+    private MessageSender bookRequestMessageSender;
+    
+    @Autowired
+    @Qualifier("bookRegistryResponseMessageHandler")
+    private MessageReceiver bookResponseMessageHandler;
+    
+    @Autowired
+    private CastorMarshaller marshaller;
+    
+    @Test
+    @Override
+    public void runTest(ITestContext testContext) {
+        super.runTest(testContext);
+    }
+    
+    @BeforeSuite(alwaysRun = true)
+    public void beforeSuite(ITestContext testContext) throws Exception {
+        getDemo().start();
+        
+        super.beforeSuite(testContext);
+    }
+    
+    @AfterSuite(alwaysRun = true)
+    public void afterSuite(ITestContext testContext) {
+        super.afterSuite(testContext);
+        
+        getDemo().stop();
+    }
+    
+    @Override
+    protected void configure() {
+        String isbn = "978-0596517335";
+        
+        send()
+            .with(bookRequestMessageSender)
+            .payload(createAddBookRequestMessage(isbn), marshaller)
+            .header("citrus_soap_action", "addBook");
+        
+        receive()
+            .with(bookResponseMessageHandler)
+            .validationCallback(new AbstractValidationCallback<AddBookResponseMessage>(marshaller) {
+                @Override
+                public void validate(AddBookResponseMessage response, MessageHeaders headers) {
+                    Assert.assertTrue(response.isSuccess());
+                }
+            });
+    }
+
+    /**
+     * @param isbn
+     * @return
+     */
+    private AddBookRequestMessage createAddBookRequestMessage(String isbn) {
+        AddBookRequestMessage requestMessage = new AddBookRequestMessage();
+        Book book = new Book();
+        book.setAuthor("Mike Loukides, Sonatype");
+        book.setTitle("Maven: The Definitive Guide");
+        book.setIsbn(isbn);
+        book.setYear(2008);
+        book.setRegistrationDate(new Date());
+        requestMessage.setBook(book);
+        return requestMessage;
+    }
+    
+    /**
+     * Gets the demo application.
+     * @return
+     */
+    public CitrusSamplesDemo getDemo() {
+        return demo;
+    }
+
+}
