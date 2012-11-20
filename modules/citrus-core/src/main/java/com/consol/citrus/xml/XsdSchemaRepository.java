097d9df7c5a06eca9069870876fb57d1a2f0b078
christophd <deppisch@consol.de>
2012-11-20 11:27:30 +0100

    Have to call after properties set on newly created schema instances

--- modules/citrus-core/src/main/java/com/consol/citrus/xml/XsdSchemaRepository.java
+++ modules/citrus-core/src/main/java/com/consol/citrus/xml/XsdSchemaRepository.java
@@ -90,10 +90,14 @@ public class XsdSchemaRepository implements BeanNameAware, InitializingBean {
             for (Resource resource : findings) {
                 if (resource.getFilename().endsWith(".xsd")) {
                     log.info("Loading XSD schema resource " + resource.getFile().getAbsolutePath());
-                    schemas.add(new SimpleXsdSchema(resource));
+                    SimpleXsdSchema schema = new SimpleXsdSchema(resource);
+                    schema.afterPropertiesSet();
+                    schemas.add(schema);
                 } else if (resource.getFilename().endsWith(".wsdl")) {
                     log.info("Loading WSDL schema resource " + resource.getFile().getAbsolutePath());
-                    schemas.add(new WsdlXsdSchema(resource));
+                    WsdlXsdSchema wsdl = new WsdlXsdSchema(resource);
+                    wsdl.afterPropertiesSet();
+                    schemas.add(wsdl);
                 } else {
                     log.warn("Skipped resource other than XSD schema for repository (" + resource.getFilename() + ")");
                 }
