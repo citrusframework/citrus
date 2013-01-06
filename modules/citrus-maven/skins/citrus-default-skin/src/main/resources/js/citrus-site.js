4a7e017a1d38d7c8e69e74d58d0ea71713cb7d9c
christophd <deppisch@consol.de>
2013-01-06 19:58:01 +0100

    Use new site layout with html instead of apt files

--- modules/citrus-maven/skins/citrus-default-skin/src/main/resources/js/citrus-site.js
+++ modules/citrus-maven/skins/citrus-default-skin/src/main/resources/js/citrus-site.js
@@ -1,7 +1,3 @@
 $(document).ready(function () {
-    $('#newsfeed').rssfeed('http://labs.consol.de/tags/citrus/feed/', {
-      header: false,
-      snippet: false,
-      limit: 3
-    });
+    prettyPrint();
 });
--- src/site/resources/js/newsfeed.js
+++ src/site/resources/js/newsfeed.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     $('#newsfeed').rssfeed('http://labs.consol.de/tags/citrus/feed/', {
       header: false,
       snippet: false,
-      limit: 5
+      limit: 3
     });
 });
