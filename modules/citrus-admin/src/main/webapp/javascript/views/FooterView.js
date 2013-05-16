bca36aa1da9b0aa90a7793d8d1edd0d8fe4c342f
christophd <deppisch@consol.de>
2013-05-16 18:56:39 +0200

    Made footer content resizable to half window size on dblclick

--- modules/citrus-admin/src/main/webapp/javascript/views/FooterView.js
+++ modules/citrus-admin/src/main/webapp/javascript/views/FooterView.js
@@ -1,13 +1,21 @@
 (function() {
     define(["TemplateManager", "views/LoggerView"], function(TemplateManager, LoggerView) {
         var FooterView = Backbone.View.extend({
-        
+          events: {
+              "dblclick #footer-content": "resize"
+          },
+          
           render: function() {
               $(this.el).html(TemplateManager.template('FooterView',{}));
 
               $('#logger').html(this.getLoggerView().render().el);
               
               return this;
+          },
+          
+          resize: function() {
+              $('#footer').toggleClass('resized');
+              $('pre.log').toggleClass('resized');
           },
         
           getLoggerView: function() {
