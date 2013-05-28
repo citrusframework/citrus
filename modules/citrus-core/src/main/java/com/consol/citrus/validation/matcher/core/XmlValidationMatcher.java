e88bc1b785b67beb45b4d1a68a0dbb3480292ad1
christophd <deppisch@consol.de>
2013-05-28 10:40:02 +0200

    Fixed missing import on logger factory

--- modules/citrus-core/src/main/java/com/consol/citrus/validation/matcher/core/XmlValidationMatcher.java
+++ modules/citrus-core/src/main/java/com/consol/citrus/validation/matcher/core/XmlValidationMatcher.java
@@ -1,6 +1,7 @@
 package com.consol.citrus.validation.matcher.core;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
