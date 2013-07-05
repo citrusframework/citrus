dac0102debc4892a58f2d9c82bb372c85cdd6408
christophd <deppisch@consol.de>
2013-07-05 12:56:08 +0200

    Added xsd schema repository configuration management

--- modules/citrus-admin/src/main/java/com/consol/citrus/admin/controller/ConfigurationController.java
+++ modules/citrus-admin/src/main/java/com/consol/citrus/admin/controller/ConfigurationController.java
@@ -16,15 +16,21 @@
 
 package com.consol.citrus.admin.controller;
 
+import com.consol.citrus.admin.service.SpringBeanService;
+import com.consol.citrus.model.config.core.SchemaRepository;
+import com.consol.citrus.model.config.core.XsdSchema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.consol.citrus.admin.service.ConfigurationService;
 
+import java.util.List;
+
 /**
- * Project controller handles project choosing requests and project configuration setup
- * form submits
+ * Configuration controller handles project choosing requests and project configuration setup
+ * form submits. In addition to that manages spring bean configuration with adding/updating/deleting beans
+ * from application context.
  * 
  * @author Christoph Deppisch
  */
@@ -34,6 +40,9 @@ public class ConfigurationController {
 
     @Autowired
     private ConfigurationService configService;
+
+    @Autowired
+    private SpringBeanService springBeanService;
 
     @RequestMapping(value = "/projecthome", method = RequestMethod.GET)
     @ResponseBody
@@ -45,5 +54,65 @@ public class ConfigurationController {
     @ResponseBody
     public String getRootDirectory() {
         return configService.getRootDirectory();
+    }
+
+    @RequestMapping(value = "/xsd-schema-repository", method = {RequestMethod.GET})
+    @ResponseBody
+    public List<SchemaRepository> listXsdSchemaRepositories() {
+        return springBeanService.getBeanDefinitions(configService.getProjectConfigFile(), SchemaRepository.class);
+    }
+
+    @RequestMapping(value = "/xsd-schema-repository/{id}", method = {RequestMethod.GET})
+    @ResponseBody
+    public SchemaRepository getXsdSchemaRepository(@PathVariable("id") String id) {
+        return springBeanService.getBeanDefinition(configService.getProjectConfigFile(), id, SchemaRepository.class);
+    }
+
+    @RequestMapping(value="/xsd-schema-repository", method = {RequestMethod.POST})
+    @ResponseBody
+    public void createXsdSchemaRepository(@RequestBody SchemaRepository xsdSchemaRepository) {
+        springBeanService.addBeanDefinition(configService.getProjectConfigFile(), xsdSchemaRepository);
+    }
+
+    @RequestMapping(value = "/xsd-schema-repository/{id}", method = {RequestMethod.PUT})
+    @ResponseBody
+    public void updateXsdSchemaRepository(@PathVariable("id") String id, @RequestBody SchemaRepository xsdSchemaRepository) {
+        springBeanService.updateBeanDefinition(configService.getProjectConfigFile(), id, xsdSchemaRepository);
+    }
+
+    @RequestMapping(value = "/xsd-schema-repository/{id}", method = {RequestMethod.DELETE})
+    @ResponseBody
+    public void deleteXsdSchemaRepository(@PathVariable("id") String id) {
+        springBeanService.removeBeanDefinition(configService.getProjectConfigFile(), id);
+    }
+
+    @RequestMapping(value = "/xsd-schema", method = {RequestMethod.GET})
+    @ResponseBody
+    public List<XsdSchema> listXsdSchemas() {
+        return springBeanService.getBeanDefinitions(configService.getProjectConfigFile(), XsdSchema.class);
+    }
+
+    @RequestMapping(value = "/xsd-schema/{id}", method = {RequestMethod.GET})
+    @ResponseBody
+    public XsdSchema getXsdSchema(@PathVariable("id") String id) {
+        return springBeanService.getBeanDefinition(configService.getProjectConfigFile(), id, XsdSchema.class);
+    }
+
+    @RequestMapping(value="/xsd-schema", method = {RequestMethod.POST})
+    @ResponseBody
+    public void createXsdSchema(@RequestBody XsdSchema xsdSchema) {
+        springBeanService.addBeanDefinition(configService.getProjectConfigFile(), xsdSchema);
+    }
+
+    @RequestMapping(value = "/xsd-schema/{id}", method = {RequestMethod.PUT})
+    @ResponseBody
+    public void updateXsdSchema(@PathVariable("id") String id, @RequestBody XsdSchema xsdSchema) {
+        springBeanService.updateBeanDefinition(configService.getProjectConfigFile(), id, xsdSchema);
+    }
+
+    @RequestMapping(value = "/xsd-schema/{id}", method = {RequestMethod.DELETE})
+    @ResponseBody
+    public void deleteXsdSchema(@PathVariable("id") String id) {
+        springBeanService.removeBeanDefinition(configService.getProjectConfigFile(), id);
     }
 }
--- modules/citrus-admin/src/main/webapp/javascript/views/ConfigXsdSchemaView.js
+++ modules/citrus-admin/src/main/webapp/javascript/views/ConfigXsdSchemaView.js
@@ -1,15 +1,28 @@
 (function () {
     define(["TemplateManager"], function (TemplateManager) {
         var ConfigXsdSchemaView = Backbone.View.extend({
+            schemas: undefined,
+            schemaRepositories: undefined,
+
             events: {
                 "click #btn-new-config-xsd-schema": "showNewXsdSchemaForm",
                 "click #btn-save-config-xsd-schema": "createXsdSchema",
                 "click #btn-update-config-xsd-schema": "updateXsdSchema",
                 "click #btn-cancel-config-xsd-schema": "closeXsdSchemaForm",
+                "click #btn-new-config-schema-repository": "showNewXsdSchemaRepositoryForm",
+                "click #btn-save-config-schema-repository": "createXsdSchemaRepository",
+                "click #btn-update-config-schema-repository": "updateXsdSchemaRepository",
+                "click #btn-cancel-config-schema-repository": "closeXsdSchemaRepositoryForm",
                 "click .edit-config-xsd-schema": "showEditXsdSchemaForm",
                 "click .del-config-xsd-schema": "removeXsdSchema",
+                "click .edit-config-schema-repository": "showEditXsdSchemaRepositoryForm",
+                "click .del-config-schema-repository": "removeXsdSchemaRepository",
                 "submit #form-filter-config-xsd-schemas": "filterXsdSchemas",
-                "click #btn-reload-config-xsd-schemas": "getXsdSchemas"
+                "click #btn-reload-config-xsd-schemas": "reload",
+                "click #btn-reload-config-schema-repository": "reload",
+                "click a.xsd-schema-select": "selectXsdSchema",
+                "click #btn-add-config-xsd-schema-ref": "addXsdSchemaReference",
+                "click input[name='schema-ref']": "showXsdSchemaReferenceSelect"
             },
 
             initialize: function () {
@@ -21,7 +34,16 @@
             },
 
             afterRender: function () {
+                $('#input-filter-config-xsd-schemas').keyup(_.bind(function () {
+                    this.filterXsdSchemas();
+                }, this));
+
+                this.reload();
+            },
+
+            reload: function() {
                 this.getXsdSchemas();
+                this.getXsdSchemaRepositories();
             },
 
             getXsdSchemas: function () {
@@ -31,12 +53,21 @@
                     dataType: "json",
                     success: _.bind(function (response) {
                         this.schemas = response;
-                        $('#input-filter-config-xsd-schemas').keyup(_.bind(function () {
-                            this.filterXsdSchemas();
-                        }, this));
-
-                        $("#config-xsd-schemas-table").html(TemplateManager.template('ConfigXsdSchemaTableView', {matches: this.schemas}));
+                        $("#config-xsd-schemas-table").html(TemplateManager.template('XsdSchemaTableView', {matches: this.schemas}));
                         $('#input-filter-config-xsd-schemas').val('');
+                    }, this),
+                    async: true
+                });
+            },
+
+            getXsdSchemaRepositories: function () {
+                $.ajax({
+                    url: "config/xsd-schema-repository",
+                    type: 'GET',
+                    dataType: "json",
+                    success: _.bind(function (response) {
+                        this.schemaRepositories = response;
+                        $("#config-schema-repository-table").html(TemplateManager.template('XsdSchemaRepositoryTableView', {matches: this.schemaRepositories}));
                     }, this),
                     async: true
                 });
@@ -46,14 +77,14 @@
                 var searchKey = $('#input-filter-config-xsd-schemas').val();
 
                 if (searchKey.length) {
-                    $("#config-xsd-schemas-table").html(TemplateManager.template('ConfigXsdSchemaTableView', {matches: _.filter(this.schemas, function (schema) {
+                    $("#config-xsd-schemas-table").html(TemplateManager.template('XsdSchemaTableView', {matches: _.filter(this.schemas, function (schema) {
                         // regex: match all key names (e.g. "keyname":) in JSON String
                         var regex = /"(\w|0-9|_)+":/;
                         // replace key names with empty string and only search for match in values
                         return JSON.stringify(schema).replace(regex,"").indexOf(searchKey) >= 0;
                     })}));
                 } else {
-                    $("#config-xsd-schemas-table").html(TemplateManager.template('ConfigXsdSchemaTableView', {matches: this.schemas}));
+                    $("#config-xsd-schemas-table").html(TemplateManager.template('XsdSchemaTableView', {matches: this.schemas}));
                 }
 
                 return false;
@@ -68,7 +99,23 @@
                     url: url,
                     type: 'DELETE',
                     success: _.bind(function (response) {
-                        this.getXsdSchemas();
+                        this.reload();
+                    }, this),
+                    async: true
+                });
+
+            },
+
+            removeXsdSchemaRepository: function (event) {
+                var encodedId = $(event.target).closest($("[id]")).attr('id');
+                var id = this.extractId(encodedId);
+                var url = "config/xsd-schema-repository/" + id;
+
+                $.ajax({
+                    url: url,
+                    type: 'DELETE',
+                    success: _.bind(function (response) {
+                        this.reload();
                     }, this),
                     async: true
                 });
@@ -76,8 +123,14 @@
             },
 
             showNewXsdSchemaForm: function() {
-                $('#dialog-edit-config-xsd-schema').html(TemplateManager.template('ConfigXsdSchemaEditView', {schema: undefined}));
+                $('#dialog-edit-config-xsd-schema').html(TemplateManager.template('XsdSchemaEditView', {schema: undefined}));
                 $('#dialog-edit-config-xsd-schema .modal').modal();
+
+            },
+
+            showNewXsdSchemaRepositoryForm: function() {
+                $('#dialog-edit-config-schema-repository').html(TemplateManager.template('XsdSchemaRepositoryEditView', {schemaRepository: undefined, schemas: this.schemas}));
+                $('#dialog-edit-config-schema-repository .modal').modal();
 
             },
 
@@ -91,8 +144,25 @@
                     type: 'GET',
                     dataType: "json",
                     success: _.bind(function (response) {
-                        $('#dialog-edit-config-xsd-schema').html(TemplateManager.template('ConfigXsdSchemaEditView', {schema: response}));
+                        $('#dialog-edit-config-xsd-schema').html(TemplateManager.template('XsdSchemaEditView', {schema: response}));
                         $('#dialog-edit-config-xsd-schema .modal').modal();
+                    }, this),
+                    async: true
+                });
+            },
+
+            showEditXsdSchemaRepositoryForm: function() {
+                var encodedSchemaId = $(event.target).closest($("[id]")).attr('id');
+                var id = this.extractId(encodedSchemaId);
+                var url = "config/xsd-schema-repository/" + id;
+
+                $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: "json",
+                    success: _.bind(function (response) {
+                        $('#dialog-edit-config-schema-repository').html(TemplateManager.template('XsdSchemaRepositoryEditView', {schemaRepository: response, schemas: this.schemas}));
+                        $('#dialog-edit-config-schema-repository .modal').modal();
                     }, this),
                     async: true
                 });
@@ -112,7 +182,27 @@
                     contentType: "application/json",
                     data: jsonForm,
                     success: _.bind(function (response) {
-                        this.getXsdSchemas();
+                        this.reload();
+                    }, this),
+                    async: true
+                });
+                return false;
+            },
+
+            createXsdSchemaRepository: function() {
+                var form = $('#form-edit-config-schema-repository form');
+                this.closeXsdSchemaRepositoryForm();
+
+                var serializedForm = form.serializeObject();
+                var url = "config/xsd-schema-repository";
+                $.ajax({
+                    url: url,
+                    type: 'POST',
+                    dataType: "json",
+                    contentType: "application/json",
+                    data: this.getSchemaRepositoryJSON(serializedForm),
+                    success: _.bind(function (response) {
+                        this.reload();
                     }, this),
                     async: true
                 });
@@ -133,20 +223,80 @@
                     contentType: "application/json",
                     data: jsonForm,
                     success: _.bind(function (response) {
-                        this.getXsdSchemas();
+                        this.reload();
                     }, this),
                     async: true
                 });
                 return false;
             },
 
+            updateXsdSchemaRepository: function() {
+                var form = $('#form-edit-config-schema-repository form');
+                this.closeXsdSchemaRepositoryForm();
+
+                var serializedForm = form.serializeObject();
+                var url = "config/xsd-schema-repository/" + serializedForm.id;
+                $.ajax({
+                    url: url,
+                    type: 'PUT',
+                    dataType: "json",
+                    contentType: "application/json",
+                    data: this.getSchemaRepositoryJSON(serializedForm),
+                    success: _.bind(function (response) {
+                        this.reload();
+                    }, this),
+                    async: true
+                });
+                return false;
+            },
+
+            getSchemaRepositoryJSON: function(serializedForm) {
+                var schemas = [];
+
+                $('ul#xsd-schema-refs').children('li').each(function(index) {
+                    schemas.push({ ref: $(this).attr('id') });
+                });
+
+                var schemaRepository = { id: serializedForm.id, schemas: { schemas: schemas } };
+
+                return JSON.stringify(schemaRepository);
+            },
+
             closeXsdSchemaForm: function() {
                 $('#dialog-edit-config-xsd-schema .modal').modal('hide');
+            },
+
+            closeXsdSchemaRepositoryForm: function() {
+                $('#dialog-edit-config-schema-repository .modal').modal('hide');
             },
 
             extractId: function(encodedId) {
                 var splitString = encodedId.split('-');
                 return splitString[splitString.length-1];
+            },
+
+            selectXsdSchema: function(event) {
+                $('input[name="schema-ref"]').val(event.currentTarget.innerText);
+                $('#xsd-schema-ref-dropdown').removeClass('open');
+                return false;
+            },
+
+            addXsdSchemaReference: function() {
+                var schemaId = $('input[name="schema-ref"]').val();
+                if (schemaId.length) {
+                    if ($('ul#xsd-schema-refs').children('li#' + schemaId).size() == 0) {
+                        $('ul#xsd-schema-refs').append('<li id="' + schemaId + '"><i class="icon-file-text-alt"></i>&nbsp;' + schemaId + '</li>');
+                    }
+
+                    $('input[name="schema-ref"]').val('');
+                }
+
+                return false;
+            },
+
+            showXsdSchemaReferenceSelect: function(event) {
+                $('#xsd-schema-ref-dropdown').addClass('open');
+                event.stopPropagation();
             }
 
         });
