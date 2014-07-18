3bdf6280676ce07c31e8de68f0b5df28f97ca5ce
christophd <deppisch@consol.de>
2014-07-18 11:16:47 +0200

    Moved citrus-samples to separate git repo

--- modules/citrus-samples/greeting/service/src/main/script/greetingDemo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-JARS=`find lib ../lib ../../lib ../../dist -name *.jar`
-CP=`echo $JARS | sed 's/ /:/g'`
-java -cp $CP:src/citrus/resources com.consol.citrus.samples.greeting.jms.GreetingJmsDemo $*
