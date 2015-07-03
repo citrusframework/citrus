d488c404d22fad1b2c80485e881a24e3d7c87259
christophd <deppisch@consol.de>
2015-07-03 08:45:36 +0200

    Update copyright Apache license for file

--- modules/citrus-admin/src/main/java/com/consol/citrus/admin/websocket/LoggingWebSocketCreator.java
+++ modules/citrus-admin/src/main/java/com/consol/citrus/admin/websocket/LoggingWebSocketCreator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2006-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.consol.citrus.admin.websocket;
 
 import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
@@ -6,7 +22,8 @@ import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * Created by Martin.Maher@consol.de on 19/06/15.
+ * @author Martin Maher
+ * @since 2.2.1
  */
 public class LoggingWebSocketCreator implements WebSocketCreator {
 
