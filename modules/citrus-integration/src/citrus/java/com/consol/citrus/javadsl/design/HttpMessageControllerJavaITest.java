a20c3da1eef5d51c6b6624477b3e99103fff14f2
christophd <deppisch@consol.de>
2015-07-09 17:04:34 +0200

    Moved test designer test to accurate package

---
+++ modules/citrus-integration/src/citrus/java/com/consol/citrus/javadsl/design/HttpMessageControllerJavaITest.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2006-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.javadsl.design;
+
+import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.http.message.HttpMessage;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.testng.annotations.Test;
+
+/**
+ * @author Christoph Deppisch
+ */
+@Test
+public class HttpMessageControllerJavaITest extends TestNGCitrusTestDesigner {
+    
+    @CitrusTest(name = "HttpMessageControllerJavaITest")
+    public void httpMessageControllerITest() {
+        variable("id", "123456789");
+        
+        echo("First request without query parameter and context path variables.");
+        
+        parallel(
+            send("httpClient")
+                .http()
+                .uri("http://localhost:8072")
+                .message(new HttpMessage()
+                    .method(HttpMethod.GET)
+                    .contentType("text/html")
+                    .accept("application/xml;charset=UTF-8")),
+                
+            sequential(
+                receive("httpServerRequestEndpoint")
+                    .message(new HttpMessage()
+                        .method(HttpMethod.GET)
+                        .contentType("text/html")
+                        .header("Host", "localhost:8072")
+                        .accept("application/xml;charset=UTF-8"))
+                    .http().uri("/").contextPath("")
+            )
+        );
+        
+        receive("httpClient")
+            .timeout(2000L)
+            .http()
+            .status(HttpStatus.OK)
+            .version("HTTP/1.1");
+
+        
+        echo("Use context path variables.");
+        
+        parallel(
+            send("httpClient")
+                .http()
+                .uri("http://localhost:8072/test/user/${id}")
+                .message(new HttpMessage()
+                    .method(HttpMethod.GET)
+                    .contentType("text/html")
+                    .accept("application/xml;charset=UTF-8")),
+
+            sequential(
+                receive("httpServerRequestEndpoint")
+                    .http()
+                    .message(new HttpMessage()
+                        .contentType("text/html")
+                        .method(HttpMethod.GET)
+                        .header("Host", "localhost:8072")
+                        .accept("application/xml;charset=UTF-8"))
+                    .uri("/test/user/${id}")
+                    .contextPath("")
+            )
+        );
+        
+        receive("httpClient")
+            .timeout(2000L)
+            .http()
+            .status(HttpStatus.OK)
+            .version("HTTP/1.1");
+        
+        echo("Use query parameter and context path variables.");
+        
+        parallel(
+            send("httpClient")
+                .http()
+                .uri("http://localhost:8072/test")
+                .message(new HttpMessage()
+                    .method(HttpMethod.GET)
+                    .contentType("text/html")
+                    .queryParam("id", "${id}")
+                    .queryParam("name", "TestUser")
+                    .accept("application/xml;charset=UTF-8"))
+                .path("user"),
+
+            sequential(
+                receive("httpServerRequestEndpoint")
+                    .http()
+                    .message(new HttpMessage()
+                        .method(HttpMethod.GET)
+                        .contentType("text/html")
+                        .header("Host", "localhost:8072")
+                        .accept("application/xml;charset=UTF-8"))
+                    .uri("/test/user")
+                    .contextPath("")
+                    .queryParam("id", "${id}")
+                    .queryParam("name", "TestUser")
+            )
+        );
+
+        receive("httpClient")
+            .timeout(2000L)
+            .http()
+            .status(HttpStatus.OK)
+            .version("HTTP/1.1");
+    }
+}
---
+++ modules/citrus-integration/src/citrus/java/com/consol/citrus/javadsl/design/IterateJavaITest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2006-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.javadsl.design;
+
+import com.consol.citrus.actions.AbstractTestAction;
+import com.consol.citrus.context.TestContext;
+import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
+import com.consol.citrus.annotations.CitrusTest;
+import org.testng.annotations.Test;
+
+/**
+ * @author Christoph Deppisch
+ */
+@Test
+public class IterateJavaITest extends TestNGCitrusTestDesigner {
+    
+    @CitrusTest
+    public void IterateJavaITest() {
+        variable("max", "3");
+        
+        iterate(echo("index is: ${i}")).condition("i lt= citrus:randomNumber(1)").index("i");
+        
+        iterate(echo("index is: ${i}")).condition("i lt 20").index("i");
+        
+        iterate(echo("index is: ${i}")).condition("(i lt 5) or (i = 5)").index("i");
+        
+        iterate(echo("index is: ${i}")).condition("(i lt 5) and (i lt 3)").index("i");
+        
+        iterate(echo("index is: ${i}")).condition("i = 0").index("i");
+        
+        iterate(echo("index is: ${i}")).condition("${max} gt= i").index("i");
+
+        iterate(echo("index is: ${i}")).condition("i lt= 50").index("i")
+                                       .startsWith(0)
+                                       .step(5);
+
+        AbstractTestAction anonymous = new AbstractTestAction() {
+            @Override
+            public void doExecute(TestContext context) {
+                log.info(context.getVariable("index"));
+            }
+        };
+
+        iterate(variables().add("index", "${i}"), anonymous).condition("i lt 5").index("i");
+    }
+}
