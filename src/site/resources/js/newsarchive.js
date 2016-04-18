76bee83455753bd350bbc963294689bd435e67a3
christophd <deppisch@consol.de>
2016-04-18 08:57:07 +0200

    Fixed #93 Links to blogs on front page is 404 - now using plain jquery instead of discontinued zrss library

--- src/site/resources/js/newsarchive.js
+++ src/site/resources/js/newsarchive.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
-    $('#newsfeed').rssfeed('http://labs.consol.de/tags/citrus/feed/', {
-      header: false,
-      snippet: false
-    });
+    $.get('news/feed', '', function (data) {
+        $(data).find("entry").each(function () {
+            var entry = $(this);
+
+            if (entry.find("title").text().indexOf("Citrus") > -1 || entry.find("content").text().indexOf("Citrus") > -1 || entry.find("content").text().indexOf("citrus") > -1) {
+                $('ul.newsfeed').append('<li class="rssRow"><h3> <a target="_blank"' +
+                    'title="' + entry.find("title").text() +
+                    '" href="' + entry.find("link").attr("href") + '">' +
+                    entry.find("title").text() +
+                    '</a></h3><div>' + entry.find("published").text() +
+                    '</div>' + $('<div/>').html(entry.find("content").text()).text() + ' <a target="_blank"' +
+                    'title="' + entry.find("title").text() +
+                    '" href="' + entry.find("link").attr("href") + '">[more]</a></li>');
+            }
+        });
+
+        return false;
+    }, 'xml');
 });
--- src/site/resources/js/newsfeed.js
+++ src/site/resources/js/newsfeed.js
@@ -1,8 +1,21 @@
 $(document).ready(function () {
-    $('#newsfeed').rssfeed('http://labs.consol.de/tags/citrus/feed/', {
-      header: false,
-      snippet: false,
-      date: false,
-      limit: 4
-    });
+    $.get('news/feed', '', function (data) {
+        var entries = 0;
+
+        $(data).find("entry").each(function () {
+            var entry = $(this);
+
+            if (entries < 5 && (entry.find("title").text().indexOf("Citrus") > -1 || entry.find("content").text().indexOf("Citrus") > -1 || entry.find("content").text().indexOf("citrus") > -1)) {
+                $('ul.newsfeed').append('<li class="rssRow"><h4> <a target="_blank"' +
+                    'title="' + entry.find("title").text() +
+                    '" href="' + entry.find("link").attr("href") + '">' +
+                    entry.find("title").text() +
+                    '</a></h4>' + $('<div/>').html(entry.find("content").text()).text().substring(0, 100) + ' ...</li>');
+
+                entries++;
+            }
+        });
+
+        return false;
+    }, 'xml');
 });
