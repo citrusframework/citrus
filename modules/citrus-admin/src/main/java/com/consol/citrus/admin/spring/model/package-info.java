80aeacf41258a694c6b587a4a8c1491bbe223623
christophd <deppisch@consol.de>
2016-04-29 13:36:20 +0200

    Removed citrus-admin module as it has moved to separate repository

--- modules/citrus-admin/src/main/java/com/consol/citrus/admin/spring/model/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.springframework.org/schema/beans", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.consol.citrus.admin.spring.model;
