45448f639bd080b0078348c2e508d42c4b4aa63d
christophd <deppisch@consol.de>
2017-03-07 20:03:12 +0100

    #217 Cucumber Http steps

---
+++ modules/citrus-cucumber/src/main/java/com/consol/citrus/cucumber/step/designer/http/HttpSteps.java
@@ -0,0 +1,306 @@
+/*
+ * Copyright 2006-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.cucumber.step.designer.http;
+
+import com.consol.citrus.Citrus;
+import com.consol.citrus.annotations.CitrusFramework;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.dsl.builder.*;
+import com.consol.citrus.dsl.design.TestDesigner;
+import com.consol.citrus.exceptions.CitrusRuntimeException;
+import com.consol.citrus.http.client.HttpClient;
+import com.consol.citrus.http.message.HttpMessage;
+import com.consol.citrus.http.server.HttpServer;
+import cucumber.api.Scenario;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.*;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Christoph Deppisch
+ * @since 2.7
+ */
+public class HttpSteps {
+
+    @CitrusResource
+    private TestDesigner designer;
+
+    @CitrusFramework
+    private Citrus citrus;
+
+    protected HttpClient httpClient;
+    protected HttpServer httpServer;
+
+    private HttpMessage request;
+    private HttpMessage response;
+
+    private Map<String, String> headers;
+
+    private String body;
+    private String contentType;
+
+    private String url;
+
+    @Before
+    public void before(Scenario scenario) {
+        if (httpClient == null && citrus.getApplicationContext().getBeansOfType(HttpClient.class).size() == 1L) {
+            httpClient = citrus.getApplicationContext().getBean(HttpClient.class);
+        }
+
+        if (httpServer == null && citrus.getApplicationContext().getBeansOfType(HttpServer.class).size() == 1L) {
+            httpServer = citrus.getApplicationContext().getBean(HttpServer.class);
+        }
+
+        request = new HttpMessage();
+        response = new HttpMessage();
+        headers = new HashMap<>();
+    }
+
+    @Given("^http-client \"([^\"\\s]+)\"$")
+    public void setClient(String id) {
+        if (!citrus.getApplicationContext().containsBean(id)) {
+            throw new CitrusRuntimeException("Unable to find http client for id: " + id);
+        }
+
+        httpClient = citrus.getApplicationContext().getBean(id, HttpClient.class);
+    }
+
+    @Given("^http-server \"([^\"\\s]+)\"$")
+    public void setServer(String id) {
+        if (!citrus.getApplicationContext().containsBean(id)) {
+            throw new CitrusRuntimeException("Unable to find http server for id: " + id);
+        }
+
+        httpServer = citrus.getApplicationContext().getBean(id, HttpServer.class);
+    }
+
+    @Given("^URL: ([^\\s]+)$")
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Given("^Path: ([^\\s]+)$")
+    public void setRequestPath(String path) {
+        request.path(path);
+        request.contextPath(path);
+    }
+
+    @Given("^Host: (.+)$")
+    public void setHost(String host) {
+        this.headers.put("Host", host);
+    }
+
+    @Given("^Accept: (.+)$")
+    public void setAccept(String contentType) {
+        headers.put("Accept", contentType);
+    }
+
+    @Given("^Accept-Encoding: (.+)$")
+    public void setAcceptEncoding(String encoding) {
+        this.headers.put("Accept-Encoding", encoding);
+    }
+
+    @Given("^Content-Type: (.+)$")
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    @Given("^Content-Encoding: (.+)$")
+    public void setContentEncoding(String encoding) {
+        this.headers.put("Content-Encoding", encoding);
+    }
+
+    @Given("^(X-[^\\s]+): (.+)$")
+    public void addCustomHeader(String name, String value) {
+        headers.put(name, value);
+    }
+
+    @Given("^Header ([^\\s]+): (.+)$")
+    public void addHeader(String name, String value) {
+        headers.put(name, value);
+    }
+
+    @Given("^(?:Request|Response):$")
+    public void setBodyMultiline(String body) {
+        setBody(body);
+    }
+
+    @Given("^(?:Request|Response): (.+)$")
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @When("^(?:http-client )?sends? (GET|HEAD|POST|PUT|PATCH|DELETE|OPTIONS|TRACE)$")
+    public void sendClientRequest(String method) {
+        sendClientRequest(method, null);
+    }
+
+    @When("^(?:http-client )?sends? (GET|HEAD|POST|PUT|PATCH|DELETE|OPTIONS|TRACE) ([^\"\\s]+)$")
+    public void sendClientRequest(String method, String path) {
+        request.method(HttpMethod.valueOf(method));
+
+        if (StringUtils.hasText(path)) {
+            request.path(path);
+        }
+
+        if (StringUtils.hasText(body)) {
+            request.setPayload(body);
+            body = null;
+        }
+
+        if (StringUtils.hasText(contentType)) {
+            request.contentType(contentType);
+            contentType = null;
+        }
+
+        for (Map.Entry<String, String> headerEntry : headers.entrySet()) {
+            request.setHeader(headerEntry.getKey(), headerEntry.getValue());
+        }
+        headers.clear();
+
+        HttpClientActionBuilder.HttpClientSendActionBuilder sendBuilder = designer.http().client(httpClient).send();
+        HttpClientRequestActionBuilder requestBuilder;
+
+        if (request.getRequestMethod() == null || request.getRequestMethod().equals(HttpMethod.POST)) {
+            requestBuilder = sendBuilder.post().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.GET)) {
+            requestBuilder = sendBuilder.get().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.PUT)) {
+            requestBuilder = sendBuilder.put().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.DELETE)) {
+            requestBuilder = sendBuilder.delete().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.HEAD)) {
+            requestBuilder = sendBuilder.head().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.TRACE)) {
+            requestBuilder = sendBuilder.trace().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.PATCH)) {
+            requestBuilder = sendBuilder.patch().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.OPTIONS)) {
+            requestBuilder = sendBuilder.options().message(request);
+        } else {
+            requestBuilder = sendBuilder.post().message(request);
+        }
+
+        if (StringUtils.hasText(url)) {
+            requestBuilder.uri(url);
+        }
+    }
+
+    @Then("^(?:http-client )?receives? status (\\d+)(?: [^\\s]+)?$")
+    public void receiveClientResponse(Integer status) {
+        response.status(HttpStatus.valueOf(status));
+
+        if (StringUtils.hasText(body)) {
+            response.setPayload(body);
+            body = null;
+        }
+
+        if (StringUtils.hasText(contentType)) {
+            response.contentType(contentType);
+            contentType = null;
+        }
+
+        for (Map.Entry<String, String> headerEntry : headers.entrySet()) {
+            response.setHeader(headerEntry.getKey(), headerEntry.getValue());
+        }
+        headers.clear();
+
+        designer.http().client(httpClient).receive()
+                .response(response.getStatusCode())
+                .message(response);
+    }
+
+    @When("^(?:http-server )?receives? (GET|HEAD|POST|PUT|PATCH|DELETE|OPTIONS|TRACE)$")
+    public void receiveServerRequest(String method) {
+        receiveServerRequest(method, null);
+    }
+
+    @When("^(?:http-server )?receives? (GET|HEAD|POST|PUT|PATCH|DELETE|OPTIONS|TRACE) ([^\"\\s]+)$")
+    public void receiveServerRequest(String method, String path) {
+        request.method(HttpMethod.valueOf(method));
+
+        if (StringUtils.hasText(path)) {
+            request.path(path);
+        }
+
+        if (StringUtils.hasText(body)) {
+            request.setPayload(body);
+            body = null;
+        }
+
+        if (StringUtils.hasText(contentType)) {
+            request.contentType(contentType);
+            contentType = null;
+        }
+
+        for (Map.Entry<String, String> headerEntry : headers.entrySet()) {
+            request.setHeader(headerEntry.getKey(), headerEntry.getValue());
+        }
+        headers.clear();
+
+        HttpServerActionBuilder.HttpServerReceiveActionBuilder requestBuilder = designer.http().server(httpServer).receive();
+
+        if (request.getRequestMethod() == null || request.getRequestMethod().equals(HttpMethod.POST)) {
+            requestBuilder.post().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.GET)) {
+            requestBuilder.get().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.PUT)) {
+            requestBuilder.put().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.DELETE)) {
+            requestBuilder.delete().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.HEAD)) {
+            requestBuilder.head().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.TRACE)) {
+            requestBuilder.trace().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.PATCH)) {
+            requestBuilder.patch().message(request);
+        } else if (request.getRequestMethod().equals(HttpMethod.OPTIONS)) {
+            requestBuilder.options().message(request);
+        } else {
+            requestBuilder.post().message(request);
+        }
+    }
+
+    @Then("^(?:http-server )?sends? status (\\d+)(?: [^\\s]+)?$")
+    public void sendServerResponse(Integer status) {
+        response.status(HttpStatus.valueOf(status));
+
+        if (StringUtils.hasText(body)) {
+            response.setPayload(body);
+            body = null;
+        }
+
+        if (StringUtils.hasText(contentType)) {
+            response.contentType(contentType);
+            contentType = null;
+        }
+
+        for (Map.Entry<String, String> headerEntry : headers.entrySet()) {
+            response.setHeader(headerEntry.getKey(), headerEntry.getValue());
+        }
+        headers.clear();
+
+        designer.http().server(httpServer).send()
+                .response(response.getStatusCode())
+                .message(response);
+    }
+}
