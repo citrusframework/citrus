37116f1c1137b80378c73c866c13ec63a1519c41
Sven Hettwer <sven.hettwer@consol.de>
2018-07-11 14:56:52 +0200

    (#423) Added Test to reconstruct the issue

---
+++ modules/citrus-ws/src/test/java/com/consol/citrus/ws/integration/WsBasicAuthIT.java
@@ -0,0 +1,14 @@
+package com.consol.citrus.ws.integration;
+
+import com.consol.citrus.annotations.CitrusXmlTest;
+import com.consol.citrus.testng.AbstractTestNGCitrusTest;
+import org.testng.annotations.Test;
+
+
+public class WsBasicAuthIT extends AbstractTestNGCitrusTest {
+
+    @Test
+    @CitrusXmlTest
+    public void ClientAuthViaMessageSenderIT(){}
+
+}
