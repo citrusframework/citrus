84d04a49d4eb0ffe681f6f1707717ce78076d02d
christophd <deppisch@consol.de>
2018-09-22 09:46:51 +0200

    #152 Remove test classifier dependencies

--- modules/citrus-kafka/src/test/java/com/consol/citrus/kafka/embedded/EmbeddedKafkaServer.java
+++ modules/citrus-kafka/src/test/java/com/consol/citrus/kafka/embedded/EmbeddedKafkaServer.java
@@ -17,17 +17,20 @@
 package com.consol.citrus.kafka.embedded;
 
 import com.consol.citrus.exceptions.CitrusRuntimeException;
+import kafka.metrics.KafkaMetricsReporter;
 import kafka.server.*;
-import kafka.utils.*;
-import kafka.zk.EmbeddedZookeeper;
-import org.I0Itec.zkclient.ZkClient;
-import org.I0Itec.zkclient.exception.ZkInterruptedException;
+import kafka.utils.CoreUtils;
 import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.network.ListenerName;
 import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.apache.kafka.common.utils.Time;
+import org.apache.zookeeper.server.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -41,11 +44,14 @@ public class EmbeddedKafkaServer {
     private static Logger log = LoggerFactory.getLogger(EmbeddedKafkaServer.class);
 
     /** Zookeeper embedded server and client */
-    private EmbeddedZookeeper zookeeper;
-    private ZkClient zookeeperClient;
+    private ZooKeeperServer zookeeper;
+    private ServerCnxnFactory serverFactory;
 
     /** Running Kafka server instance */
     private KafkaServer kafkaServer;
+
+    /** Kafka server port */
+    private int zookeeperPort = 21181;
 
     /** Kafka server port */
     private int port = 9092;
@@ -59,38 +65,36 @@ public class EmbeddedKafkaServer {
     /** Kafka broker server properties */
     private Map<String, String> brokerProperties = Collections.emptyMap();
 
-    public static void main(String[] args) {
-        EmbeddedKafkaServer server = new EmbeddedKafkaServer();
-        server.start();
-    }
-
     /**
      * Start embedded server instances for Kafka and Zookeeper.
      */
     public void start() {
-        this.zookeeper = new EmbeddedZookeeper();
+        File logDir = createLogDir();
+        zookeeper = createZookeeperServer(logDir);
+        serverFactory = createServerFactory();
 
-        String zooKeeperConnect = "localhost:" + this.zookeeper.port();
+        try {
+            serverFactory.startup(zookeeper);
+        } catch (InterruptedException | IOException e) {
+            throw new CitrusRuntimeException("Failed to start embedded zookeeper server", e);
+        }
 
-        zookeeperClient = new ZkClient(zooKeeperConnect, 6000, 6000,
-                ZKStringSerializer$.MODULE$);
-
-        Properties brokerConfigProperties = createBrokerProperties(zooKeeperConnect, port);
-        brokerConfigProperties.setProperty(KafkaConfig.PortProp(), Integer.toString(port));
-        brokerConfigProperties.setProperty(KafkaConfig.DeleteTopicEnableProp(), "true");
+        Properties brokerConfigProperties = createBrokerProperties("localhost:" + zookeeperPort, port, logDir);
         brokerConfigProperties.setProperty(KafkaConfig.ReplicaSocketTimeoutMsProp(), "1000");
         brokerConfigProperties.setProperty(KafkaConfig.ControllerSocketTimeoutMsProp(), "1000");
         brokerConfigProperties.setProperty(KafkaConfig.OffsetsTopicReplicationFactorProp(), "1");
         brokerConfigProperties.setProperty(KafkaConfig.ReplicaHighWatermarkCheckpointIntervalMsProp(), String.valueOf(Long.MAX_VALUE));
 
-        if (this.brokerProperties != null) {
-            this.brokerProperties.forEach(brokerConfigProperties::put);
+        if (brokerProperties != null) {
+            brokerProperties.forEach(brokerConfigProperties::put);
         }
 
-        kafkaServer = TestUtils.createServer(new KafkaConfig(brokerConfigProperties), Time.SYSTEM);
-        TestUtils.boundPort(kafkaServer, SecurityProtocol.PLAINTEXT);
+        kafkaServer = new KafkaServer(new KafkaConfig(brokerConfigProperties), Time.SYSTEM,
+                scala.Option.apply(null), scala.collection.JavaConversions.asScalaBuffer(Collections.<KafkaMetricsReporter>emptyList()).toList());
+        kafkaServer.startup();
+        kafkaServer.boundPort(ListenerName.forSecurityProtocol(SecurityProtocol.PLAINTEXT));
 
-        createKafkaTopics(this.topics);
+        createKafkaTopics(topics);
     }
 
     /**
@@ -114,20 +118,43 @@ public class EmbeddedKafkaServer {
             }
         }
 
-        if (zookeeperClient != null) {
+        if (serverFactory != null) {
             try {
-                this.zookeeperClient.close();
-            } catch (ZkInterruptedException e) {
-                log.warn("Failed to close Zookeeper client", e);
-            }
-        }
-
-        if (zookeeper != null) {
-            try {
-                this.zookeeper.shutdown();
+                serverFactory.shutdown();
             } catch (Exception e) {
                 log.warn("Failed to shutdown Zookeeper instance", e);
             }
+        }
+    }
+
+    /**
+     * Creates new embedded Zookeeper server.
+     * @return
+     */
+    private ZooKeeperServer createZookeeperServer(File logDir) {
+        try {
+            return new ZooKeeperServer(logDir, logDir, 2000);
+        } catch (IOException e) {
+            throw new CitrusRuntimeException("Failed to create embedded zookeeper server", e);
+        }
+    }
+
+    private File createLogDir() {
+        String dataDirectory = System.getProperty("java.io.tmpdir");
+        return new File(dataDirectory, "zookeeper").getAbsoluteFile();
+    }
+
+    /**
+     * Create server factory for embedded Zookeeper server instance.
+     * @return
+     */
+    private ServerCnxnFactory createServerFactory() {
+        try {
+            ServerCnxnFactory serverFactory = new NIOServerCnxnFactory();
+            serverFactory.configure(new InetSocketAddress(zookeeperPort), 5000);
+            return serverFactory;
+        } catch (IOException e) {
+            throw new CitrusRuntimeException("Failed to create default zookeeper server factory", e);
         }
     }
 
@@ -140,7 +167,7 @@ public class EmbeddedKafkaServer {
         adminConfigs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:" + port);
         try (AdminClient admin = AdminClient.create(adminConfigs)) {
             List<NewTopic> newTopics = topics.stream()
-                    .map(t -> new NewTopic(t, this.partitions, (short) 1))
+                    .map(t -> new NewTopic(t, partitions, (short) 1))
                     .collect(Collectors.toList());
             CreateTopicsResult createTopics = admin.createTopics(newTopics);
             try {
@@ -155,14 +182,51 @@ public class EmbeddedKafkaServer {
      * Creates Kafka broker properties.
      * @param zooKeeperConnect
      * @param kafkaServerPort
+     * @param logDir
      * @return
      */
-    private Properties createBrokerProperties(String zooKeeperConnect, int kafkaServerPort) {
-        return TestUtils.createBrokerConfig(0, zooKeeperConnect, false,true, kafkaServerPort,
-                scala.Option.apply(null),
-                scala.Option.apply(null),
-                scala.Option.apply(null),
-                true, false, 0, false, 0, false, 0, scala.Option.apply(null), 1, false);
+    private Properties createBrokerProperties(String zooKeeperConnect, int kafkaServerPort, File logDir) {
+        Properties props = new Properties();
+
+        props.put(KafkaConfig.BrokerIdProp(), "0");
+        props.put(KafkaConfig.ZkConnectProp(), zooKeeperConnect);
+        props.put(KafkaConfig.ZkConnectionTimeoutMsProp(), "10000");
+        props.put(KafkaConfig.ReplicaSocketTimeoutMsProp(), "1500");
+        props.put(KafkaConfig.ControllerSocketTimeoutMsProp(), "1500");
+        props.put(KafkaConfig.ControlledShutdownEnableProp(), "false");
+        props.put(KafkaConfig.DeleteTopicEnableProp(), "true");
+        props.put(KafkaConfig.LogDeleteDelayMsProp(), "1000");
+        props.put(KafkaConfig.ControlledShutdownRetryBackoffMsProp(), "100");
+        props.put(KafkaConfig.LogCleanerDedupeBufferSizeProp(), "2097152");
+        props.put(KafkaConfig.LogMessageTimestampDifferenceMaxMsProp(), Long.MAX_VALUE);
+        props.put(KafkaConfig.OffsetsTopicReplicationFactorProp(), "1");
+        props.put(KafkaConfig.OffsetsTopicPartitionsProp(), "5");
+        props.put(KafkaConfig.GroupInitialRebalanceDelayMsProp(), "0");
+        props.put(KafkaConfig.LogDirProp(), logDir.getAbsolutePath());
+
+        props.put(KafkaConfig.ListenersProp(), SecurityProtocol.PLAINTEXT.name + "://localhost:" + kafkaServerPort);
+
+        props.forEach((key, value) -> System.out.println(key + "=" + value));
+
+        return props;
+    }
+
+    /**
+     * Gets the zookeeperPort.
+     *
+     * @return
+     */
+    public int getZookeeperPort() {
+        return zookeeperPort;
+    }
+
+    /**
+     * Sets the zookeeperPort.
+     *
+     * @param zookeeperPort
+     */
+    public void setZookeeperPort(int zookeeperPort) {
+        this.zookeeperPort = zookeeperPort;
     }
 
     /**
