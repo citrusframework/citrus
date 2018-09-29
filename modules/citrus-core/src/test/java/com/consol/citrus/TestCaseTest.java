96ebf0f21833fc71626e5aebb25074bd90fb4f93
Sven Hettwer <sven.hettwer@consol.de>
2018-09-29 11:26:25 +0200

    (#418) Some cleanup and method extractions

--- modules/citrus-core/src/test/java/com/consol/citrus/TestCaseTest.java
+++ modules/citrus-core/src/test/java/com/consol/citrus/TestCaseTest.java
@@ -16,7 +16,9 @@
 
 package com.consol.citrus;
 
-import com.consol.citrus.actions.*;
+import com.consol.citrus.actions.AbstractAsyncTestAction;
+import com.consol.citrus.actions.AbstractTestAction;
+import com.consol.citrus.actions.EchoAction;
 import com.consol.citrus.container.Async;
 import com.consol.citrus.context.TestContext;
 import com.consol.citrus.exceptions.CitrusRuntimeException;
@@ -26,16 +28,17 @@ import com.consol.citrus.testng.AbstractTestNGUnitTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
-/**
- * @author Christoph Deppisch
- */
 public class TestCaseTest extends AbstractTestNGUnitTest {
     
     @Test
     public void testExecution() {
-        TestCase testcase = new TestCase();
+        final TestCase testcase = new TestCase();
         testcase.setName("MyTestCase");
         
         testcase.addTestAction(new EchoAction());
@@ -45,16 +48,16 @@ public class TestCaseTest extends AbstractTestNGUnitTest {
 
     @Test
     public void testWaitForFinish() {
-        TestCase testcase = new TestCase();
+        final TestCase testcase = new TestCase();
         testcase.setName("MyTestCase");
 
         testcase.addTestAction(new EchoAction());
         testcase.addTestAction(new AbstractAsyncTestAction() {
             @Override
-            public void doExecuteAsync(TestContext context) {
+            public void doExecuteAsync(final TestContext context) {
                 try {
                     Thread.sleep(500L);
-                } catch (InterruptedException e) {
+                } catch (final InterruptedException e) {
                     throw new CitrusRuntimeException(e);
                 }
             }
@@ -65,17 +68,17 @@ public class TestCaseTest extends AbstractTestNGUnitTest {
 
     @Test(expectedExceptions = TestCaseFailedException.class, expectedExceptionsMessageRegExp = "Failed to wait for nested test actions to finish properly")
     public void testWaitForFinishTimeout() {
-        TestCase testcase = new TestCase();
+        final TestCase testcase = new TestCase();
         testcase.setTimeout(500L);
         testcase.setName("MyTestCase");
 
         testcase.addTestAction(new EchoAction());
         testcase.addTestAction(new AbstractAsyncTestAction() {
             @Override
-            public void doExecuteAsync(TestContext context) {
+            public void doExecuteAsync(final TestContext context) {
                 try {
                     Thread.sleep(1000L);
-                } catch (InterruptedException e) {
+                } catch (final InterruptedException e) {
                     throw new CitrusRuntimeException(e);
                 }
             }
@@ -86,15 +89,15 @@ public class TestCaseTest extends AbstractTestNGUnitTest {
 
     @Test
     public void testWaitForFinishAsync() {
-        TestCase testcase = new TestCase();
+        final TestCase testcase = new TestCase();
         testcase.setName("MyTestCase");
 
         testcase.addTestAction(new Async().addTestAction(new AbstractAsyncTestAction() {
             @Override
-            public void doExecuteAsync(TestContext context) {
+            public void doExecuteAsync(final TestContext context) {
                 try {
                     Thread.sleep(500L);
-                } catch (InterruptedException e) {
+                } catch (final InterruptedException e) {
                     throw new CitrusRuntimeException(e);
                 }
             }
@@ -105,10 +108,10 @@ public class TestCaseTest extends AbstractTestNGUnitTest {
     
     @Test
     public void testExecutionWithVariables() {
-        TestCase testcase = new TestCase();
+        final TestCase testcase = new TestCase();
         testcase.setName("MyTestCase");
         
-        Map<String, Object> variables = new LinkedHashMap<String, Object>();
+        final Map<String, Object> variables = new LinkedHashMap<>();
         variables.put("name", "Citrus");
         variables.put("framework", "${name}");
         variables.put("hello", "citrus:concat('Hello ', ${name}, '!')");
@@ -118,14 +121,14 @@ public class TestCaseTest extends AbstractTestNGUnitTest {
         
         testcase.addTestAction(new AbstractTestAction() {
             @Override
-            public void doExecute(TestContext context) {
+            public void doExecute(final TestContext context) {
                 Assert.assertEquals(context.getVariables().get(Citrus.TEST_NAME_VARIABLE), "MyTestCase");
                 Assert.assertEquals(context.getVariables().get(Citrus.TEST_PACKAGE_VARIABLE), TestCase.class.getPackage().getName());
                 Assert.assertEquals(context.getVariable("${name}"), "Citrus");
                 Assert.assertEquals(context.getVariable("${framework}"), "Citrus");
                 Assert.assertEquals(context.getVariable("${hello}"), "Hello Citrus!");
                 Assert.assertEquals(context.getVariable("${goodbye}"), "Goodbye Citrus!");
-                Assert.assertEquals(context.getVariable("${welcome}"), "Welcome Citrus, today is " + new CurrentDateFunction().execute(new ArrayList<String>(), context) + "!");
+                Assert.assertEquals(context.getVariable("${welcome}"), "Welcome Citrus, today is " + new CurrentDateFunction().execute(new ArrayList<>(), context) + "!");
             }
         });
         
@@ -134,15 +137,15 @@ public class TestCaseTest extends AbstractTestNGUnitTest {
     
     @Test(expectedExceptions = {TestCaseFailedException.class})
     public void testUnknownVariable() {
-        TestCase testcase = new TestCase();
+        final TestCase testcase = new TestCase();
         testcase.setName("MyTestCase");
         
         final String message = "Hello TestFramework!";
-        testcase.setVariableDefinitions(Collections.<String, Object>singletonMap("text", message));
+        testcase.setVariableDefinitions(Collections.singletonMap("text", message));
         
         testcase.addTestAction(new AbstractTestAction() {
             @Override
-            public void doExecute(TestContext context) {
+            public void doExecute(final TestContext context) {
                 Assert.assertEquals(context.getVariable("${unknown}"), message);
             }
         });
@@ -152,12 +155,12 @@ public class TestCaseTest extends AbstractTestNGUnitTest {
 
     @Test(expectedExceptions = {TestCaseFailedException.class}, expectedExceptionsMessageRegExp = "This failed in forked action")
     public void testExceptionInContext() {
-        TestCase testcase = new TestCase();
+        final TestCase testcase = new TestCase();
         testcase.setName("MyTestCase");
 
         testcase.addTestAction(new AbstractTestAction() {
             @Override
-            public void doExecute(TestContext context) {
+            public void doExecute(final TestContext context) {
                 context.addException(new CitrusRuntimeException("This failed in forked action"));
             }
         });
@@ -169,12 +172,12 @@ public class TestCaseTest extends AbstractTestNGUnitTest {
 
     @Test(expectedExceptions = {TestCaseFailedException.class})
     public void testExceptionInContextInFinish() {
-        TestCase testcase = new TestCase();
+        final TestCase testcase = new TestCase();
         testcase.setName("MyTestCase");
 
         testcase.addTestAction(new AbstractTestAction() {
             @Override
-            public void doExecute(TestContext context) {
+            public void doExecute(final TestContext context) {
                 context.addException(new CitrusRuntimeException("This failed in forked action"));
             }
         });
@@ -184,7 +187,7 @@ public class TestCaseTest extends AbstractTestNGUnitTest {
     
     @Test
     public void testFinalActions() {
-        TestCase testcase = new TestCase();
+        final TestCase testcase = new TestCase();
         testcase.setName("MyTestCase");
         
         testcase.addTestAction(new EchoAction());
@@ -195,13 +198,22 @@ public class TestCaseTest extends AbstractTestNGUnitTest {
 
     @Test
     public void testThreadLeak() {
-        TestCase testcase = new TestCase();
+
+        //GIVEN
+        final TestCase testcase = new TestCase();
         testcase.setName("ThreadLeakTestCase");
         testcase.addTestAction(new EchoAction());
+
+        //WHEN
         testcase.execute(context);
 
-        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
-        Assert.assertEquals(threadSet.stream().filter(t -> t.getName().startsWith(TestCase.FINISHER_THREAD_PREFIX)).filter(t -> t.isAlive()).count(), 0);
+        //THEN
+        final Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        Assert.assertEquals(threadSet.stream()
+                .filter(t -> t.getName().startsWith(TestCase.FINISHER_THREAD_PREFIX))
+                .filter(Thread::isAlive)
+                .count(),
+                0);
     }
 
 }
