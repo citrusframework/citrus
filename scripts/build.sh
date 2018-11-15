ef15bef6b62f08e439e223d4bf132ab4e12ddac0
Sven Hettwer <sven.hettwer@consol.de>
2018-11-15 09:25:48 +0100

    (#531) Altered pull request condition

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_SECURE_ENV_VARS" == true ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     COMMAND="mvn --batch-mode -q clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=citrus"
 else
     COMMAND="mvn --batch-mode -q clean install"
