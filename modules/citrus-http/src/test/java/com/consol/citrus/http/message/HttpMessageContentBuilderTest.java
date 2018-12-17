300be99b55b7b41f545b2be464a420b4822df2db
Sven Hettwer <sven.hettwer@consol.de>
2018-12-17 11:44:44 +0100

    (#569) Changed HttpMessageContentBuilder to use the CookieEnricher

--- modules/citrus-http/src/test/java/com/consol/citrus/http/message/HttpMessageContentBuilderTest.java
+++ modules/citrus-http/src/test/java/com/consol/citrus/http/message/HttpMessageContentBuilderTest.java
@@ -22,47 +22,68 @@ import com.consol.citrus.message.Message;
 import com.consol.citrus.message.MessageHeaders;
 import com.consol.citrus.message.MessageType;
 import com.consol.citrus.validation.builder.StaticMessageContentBuilder;
-import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.servlet.http.Cookie;
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class HttpMessageContentBuilderTest {
+
+    private HttpMessage message;
+
+    @BeforeMethod
+    public void setUp(){
+        message = new HttpMessage("");
+    }
+
+    @Test
+    public void testDefaultMessageHeader(){
+
+        //GIVEN
+        final HttpMessageContentBuilder builder = getBuilder();
+
+        //WHEN
+        final Message builtMessage = builder.buildMessageContent(new TestContext(), String.valueOf(MessageType.XML));
+
+        //THEN
+        assertEquals(builtMessage.getHeaders().entrySet().size(), 3);
+        assertEquals(message.getHeader(MessageHeaders.ID), builtMessage.getHeader(MessageHeaders.ID));
+        assertEquals(message.getHeader(MessageHeaders.TIMESTAMP), builtMessage.getHeader(MessageHeaders.TIMESTAMP));
+        assertEquals(MessageType.XML.toString(), builtMessage.getHeader(MessageHeaders.MESSAGE_TYPE));
+    }
 
     @Test
     public void testHeaderVariableSubstitution() {
 
         //GIVEN
-        final TestContext ctx = new TestContext();
-        ctx.setVariable("testHeader", "foo");
-        ctx.setVariable("testValue", "bar");
+        final HttpMessageContentBuilder builder = getBuilder();
 
-        final HttpMessage msg = new HttpMessage("testPayload");
-        msg.setHeader("${testHeader}", "${testValue}");
+        final TestContext testContext = new TestContext();
+        testContext.setVariable("testHeader", "foo");
+        testContext.setVariable("testValue", "bar");
 
-        final HttpMessageContentBuilder builder =
-                new HttpMessageContentBuilder(msg, new StaticMessageContentBuilder(msg));
+        message.setHeader("${testHeader}", "${testValue}");
 
         //WHEN
-        final Message builtMessage = builder.buildMessageContent(ctx, String.valueOf(MessageType.XML));
+        final Message builtMessage = builder.buildMessageContent(testContext, String.valueOf(MessageType.XML));
 
         //THEN
-        Assert.assertEquals(builtMessage.getHeaders().entrySet().size(), 4);
-        Assert.assertEquals(msg.getHeader(MessageHeaders.ID), builtMessage.getHeader(MessageHeaders.ID));
-        Assert.assertEquals(msg.getHeader(MessageHeaders.TIMESTAMP), builtMessage.getHeader(MessageHeaders.TIMESTAMP));
-        Assert.assertEquals(MessageType.XML.toString(), builtMessage.getHeader(MessageHeaders.MESSAGE_TYPE));
-        Assert.assertEquals(builtMessage.getHeader("foo"), "bar");
+        assertEquals(builtMessage.getHeader("foo"), "bar");
     }
 
     @Test
     public void testTemplateHeadersArePreserved(){
 
         //GIVEN
-        final HttpMessage msg = new HttpMessage("testPayload");
-        msg.setCookies(new Cookie[]{new Cookie("foo","bar")});
-
-        final HttpMessageContentBuilder builder =
-                new HttpMessageContentBuilder(msg, new StaticMessageContentBuilder(msg));
+        final HttpMessageContentBuilder builder = getBuilder();
+        message.setHeader("foo", "bar");
 
         //WHEN
         final HttpMessage builtMessage = (HttpMessage) builder.buildMessageContent(
@@ -70,8 +91,43 @@ public class HttpMessageContentBuilderTest {
                 String.valueOf(MessageType.XML));
 
         //THEN
-        Assert.assertEquals(builtMessage.getCookies().size(), 1);
-        Assert.assertEquals(builtMessage.getCookies().get(0).getName(), "foo");
-        Assert.assertEquals(builtMessage.getCookies().get(0).getValue(), "bar");
+        assertEquals(builtMessage.getHeader("foo"), "bar");
+    }
+
+    @Test
+    public void testCookieEnricherIsCalledForTemplateCookies(){
+
+        //GIVEN
+        final CookieEnricher cookieEnricherMock = mock(CookieEnricher.class);
+        final TestContext testContextMock = mock(TestContext.class);
+        final Cookie templateCookie = mock(Cookie.class);
+        message.setCookies(new Cookie[]{templateCookie});
+
+        final Cookie enrichedCookie = mock(Cookie.class);
+
+        when(cookieEnricherMock.enrich(
+                eq(Collections.singletonList(templateCookie)),
+                eq(testContextMock)))
+                .thenReturn(Collections.singletonList(enrichedCookie));
+
+        final HttpMessageContentBuilder builder = new HttpMessageContentBuilder(
+                message,
+                new StaticMessageContentBuilder(message),
+                cookieEnricherMock);
+
+        //WHEN
+        final HttpMessage message = (HttpMessage) builder.buildMessageContent(
+                testContextMock, String.valueOf(MessageType.XML));
+
+        //THEN
+        assertEquals(message.getCookies().size(), 1);
+        assertTrue(message.getCookies().contains(enrichedCookie));
+    }
+
+    private HttpMessageContentBuilder getBuilder() {
+        return new HttpMessageContentBuilder(
+                message,
+                new StaticMessageContentBuilder(message),
+                mock(CookieEnricher.class));
     }
 }
