af5352176e53549dfb72f60fd1d3f71e03bade4d
Sven Hettwer <sven.hettwer@consol.de>
2019-01-27 20:21:06 +0100

    Updated version to 2.7.10-SNAPSHOT

--- modules/citrus-arquillian/src/test/java/com/consol/citrus/arquillian/client/CitrusArchiveProcessorTest.java
+++ modules/citrus-arquillian/src/test/java/com/consol/citrus/arquillian/client/CitrusArchiveProcessorTest.java
@@ -55,7 +55,7 @@ public class CitrusArchiveProcessorTest {
     public void setCitrusVersion() {
         Field version = ReflectionUtils.findField(Citrus.class, "version");
         ReflectionUtils.makeAccessible(version);
-        ReflectionUtils.setField(version, Citrus.class, "2.7.9");
+        ReflectionUtils.setField(version, Citrus.class, "2.7.10-SNAPSHOT");
     }
 
     @BeforeMethod
