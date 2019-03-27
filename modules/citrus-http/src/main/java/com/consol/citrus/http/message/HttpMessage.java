415c535f968a0a9de0152d57ca96df321192aae8
Cliff <Cliff.Le.Van@consol.de>
2019-03-27 09:40:39 +0100

    (#583) Updated docs & removed wildcard imports

--- modules/citrus-http/src/main/java/com/consol/citrus/http/message/HttpMessage.java
+++ modules/citrus-http/src/main/java/com/consol/citrus/http/message/HttpMessage.java
@@ -31,7 +31,11 @@ import javax.servlet.http.Cookie;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
--- modules/citrus-http/src/test/java/com/consol/citrus/http/message/HttpMessageTest.java
+++ modules/citrus-http/src/test/java/com/consol/citrus/http/message/HttpMessageTest.java
@@ -25,7 +25,9 @@ import org.testng.annotations.Test;
 import javax.servlet.http.Cookie;
 
 import static org.mockito.Mockito.mock;
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
 
 public class HttpMessageTest {
 
