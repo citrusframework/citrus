9ecdfca0777404b72dc07c6864d89213bac6f850
Sven Hettwer <sven.hettwer@consol.de>
2019-04-24 15:56:53 +0200

    (#499) Apply @ignore@ matcher substitution to json values

--- modules/citrus-core/src/main/java/com/consol/citrus/validation/json/JsonTextMessageValidator.java
+++ modules/citrus-core/src/main/java/com/consol/citrus/validation/json/JsonTextMessageValidator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2011 the original author or authors.
+ * Copyright 2006-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -233,9 +233,13 @@ public class JsonTextMessageValidator extends AbstractMessageValidator<JsonMessa
                         }
                     }
                 } else {
-                    Assert.isTrue(controlValue.equals(receivedValue),
+                    final String processedControlValue = ValidationMatcherUtils.substituteIgnoreStatements(
+                                    controlValue.toString(),
+                                    receivedValue.toString());
+
+                    Assert.isTrue(processedControlValue.equals(receivedValue),
                             ValidationUtils.buildValueMismatchErrorMessage("Values not equal for entry: '" + controlKey + "'",
-                                    controlValue, receivedValue));
+                                    processedControlValue, receivedValue));
                 }
             } else if (ValidationMatcherUtils.isValidationMatcherExpression(controlValue.toString())) {
                 ValidationMatcherUtils.resolveValidationMatcher(controlKey,
--- modules/citrus-core/src/main/java/com/consol/citrus/validation/matcher/ValidationMatcherUtils.java
+++ modules/citrus-core/src/main/java/com/consol/citrus/validation/matcher/ValidationMatcherUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2010 the original author or authors.
+ * Copyright 2006-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,8 @@ import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Utility class for validation matchers.
@@ -45,15 +47,15 @@ public final class ValidationMatcherUtils {
      * @param validationMatcherExpression to evaluate.
      * @param context the test context
      */
-    public static void resolveValidationMatcher(String fieldName, String fieldValue, 
-            String validationMatcherExpression, TestContext context) {
+    public static void resolveValidationMatcher(final String fieldName, final String fieldValue,
+                                                final String validationMatcherExpression, final TestContext context) {
         String expression = VariableUtils.cutOffVariablesPrefix(cutOffValidationMatchersPrefix(validationMatcherExpression));
 
         if (expression.equals("ignore")) {
             expression += "()";
         }
 
-        int bodyStart = expression.indexOf('(');
+        final int bodyStart = expression.indexOf('(');
         if (bodyStart < 0) {
             throw new CitrusRuntimeException("Illegal syntax for validation matcher expression - missing validation value in '()' function body");
         }
@@ -63,23 +65,23 @@ public final class ValidationMatcherUtils {
             prefix = expression.substring(0, expression.indexOf(':') + 1);
         }
 
-        String matcherValue = expression.substring(bodyStart + 1, expression.length() - 1);
-        String matcherName = expression.substring(prefix.length(), bodyStart);
+        final String matcherValue = expression.substring(bodyStart + 1, expression.length() - 1);
+        final String matcherName = expression.substring(prefix.length(), bodyStart);
 
-        ValidationMatcherLibrary library = context.getValidationMatcherRegistry().getLibraryForPrefix(prefix);
-        ValidationMatcher validationMatcher = library.getValidationMatcher(matcherName);
+        final ValidationMatcherLibrary library = context.getValidationMatcherRegistry().getLibraryForPrefix(prefix);
+        final ValidationMatcher validationMatcher = library.getValidationMatcher(matcherName);
 
-        ControlExpressionParser controlExpressionParser = lookupControlExpressionParser(validationMatcher);
-        List<String> params = controlExpressionParser.extractControlValues(matcherValue, null);
-        List<String> replacedParams = replaceVariablesAndFunctionsInParameters(params, context);
+        final ControlExpressionParser controlExpressionParser = lookupControlExpressionParser(validationMatcher);
+        final List<String> params = controlExpressionParser.extractControlValues(matcherValue, null);
+        final List<String> replacedParams = replaceVariablesAndFunctionsInParameters(params, context);
         validationMatcher.validate(fieldName, fieldValue, replacedParams, context);
     }
 
-    private static List<String> replaceVariablesAndFunctionsInParameters(List<String> params, TestContext context) {
-        List<String> replacedParams = new ArrayList<>(params.size());
-        for (String param : params) {
-            String parsedVariablesParam = VariableUtils.replaceVariablesInString(param, context, false);
-            String parsedFunctionsParam = FunctionUtils.replaceFunctionsInString(parsedVariablesParam, context);
+    private static List<String> replaceVariablesAndFunctionsInParameters(final List<String> params, final TestContext context) {
+        final List<String> replacedParams = new ArrayList<>(params.size());
+        for (final String param : params) {
+            final String parsedVariablesParam = VariableUtils.replaceVariablesInString(param, context, false);
+            final String parsedFunctionsParam = FunctionUtils.replaceFunctionsInString(parsedVariablesParam, context);
             replacedParams.add(parsedFunctionsParam);
         }
         return replacedParams;
@@ -90,7 +92,7 @@ public final class ValidationMatcherUtils {
      * @param expression the expression to check
      * @return
      */
-    public static boolean isValidationMatcherExpression(String expression) {
+    public static boolean isValidationMatcherExpression(final String expression) {
         return expression.startsWith(Citrus.VALIDATION_MATCHER_PREFIX) &&
                 expression.endsWith(Citrus.VALIDATION_MATCHER_SUFFIX);
     }
@@ -100,7 +102,7 @@ public final class ValidationMatcherUtils {
      * @param expression
      * @return
      */
-    private static String cutOffValidationMatchersPrefix(String expression) {
+    private static String cutOffValidationMatchersPrefix(final String expression) {
         if (expression.startsWith(Citrus.VALIDATION_MATCHER_PREFIX) && expression.endsWith(Citrus.VALIDATION_MATCHER_SUFFIX)) {
             return expression.substring(Citrus.VALIDATION_MATCHER_PREFIX.length(), expression.length() - Citrus.VALIDATION_MATCHER_SUFFIX.length());
         }
@@ -108,14 +110,62 @@ public final class ValidationMatcherUtils {
         return expression;
     }
 
-    private static ControlExpressionParser lookupControlExpressionParser(ValidationMatcher validationMatcher) {
+    private static ControlExpressionParser lookupControlExpressionParser(final ValidationMatcher validationMatcher) {
         if (validationMatcher instanceof ControlExpressionParser) {
             return (ControlExpressionParser) validationMatcher;
         }
         return new DefaultControlExpressionParser();
     }
 
-    public static String getParameterListAsString(List<String> parameters) {
+    public static String getParameterListAsString(final List<String> parameters) {
         return StringUtils.collectionToDelimitedString(parameters, ",", "'", "'");
+    }
+
+    /**
+     * Substitutes ignore statements in the control value with the actual value from the received message at the
+     * ignore statements position. This way we can ignore words in a plaintext value.
+     * @param control The control message that potentially contains ignore statements
+     * @param substitute The message to receive the content for the substitution from.
+     *                   Mostly the message for validation, received from the endpoint.
+     * @return The control message with substituted ignore statements
+     */
+    public static String substituteIgnoreStatements(final String control, final String substitute) {
+        if (control.equals(Citrus.IGNORE_PLACEHOLDER)) {
+            return control;
+        }
+
+        final Pattern separatorPattern = Pattern.compile("[\\W]");
+        final Pattern ignorePattern = Pattern.compile("@ignore\\(?(\\d*)\\)?@");
+
+        String processedMessage = control;
+        Matcher ignoreMatcher = ignorePattern.matcher(processedMessage);
+
+        while (ignoreMatcher.find()) {
+            String actualValue;
+
+            if (ignoreMatcher.groupCount() > 0 && StringUtils.hasText(ignoreMatcher.group(1))) {
+                int end = ignoreMatcher.start() + Integer.valueOf(ignoreMatcher.group(1));
+                if (end > substitute.length()) {
+                    end = substitute.length();
+                }
+
+                if (ignoreMatcher.start() > substitute.length()) {
+                    actualValue = "";
+                } else {
+                    actualValue = substitute.substring(ignoreMatcher.start(), end);
+                }
+            } else {
+                actualValue = substitute.substring(ignoreMatcher.start());
+                final Matcher whitespaceMatcher = separatorPattern.matcher(actualValue);
+                if (whitespaceMatcher.find()) {
+                    actualValue = actualValue.substring(0, whitespaceMatcher.start());
+                }
+            }
+
+            processedMessage = ignoreMatcher.replaceFirst(actualValue);
+            ignoreMatcher = ignorePattern.matcher(processedMessage);
+        }
+
+        return processedMessage;
     }
 }
