8c535f55222c9b9ef8327ca982b9f36dd9bad76a
Sven Hettwer <sven.hettwer@consol.de>
2019-10-30 09:38:16 +0100

    (#29) exchanged PreparedStatement with CallableStatement as in sql + corrected parameter index

--- modules/citrus-integration/src/it/java/com/consol/citrus/jdbc/JdbcExecutionsIT.java
+++ modules/citrus-integration/src/it/java/com/consol/citrus/jdbc/JdbcExecutionsIT.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2006-2019 the original author or authors.
+ *  Copyright 2006-2019 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.consol.citrus.jdbc;
@@ -39,7 +39,6 @@ import java.sql.Blob;
 import java.sql.CallableStatement;
 import java.sql.Clob;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -291,11 +290,11 @@ public class JdbcExecutionsIT extends TestNGCitrusTestRunner {
                                 try {
                                     final Connection connection = jdbcDriver.connect(serverUrl, new Properties());
                                     Assert.assertNotNull(connection);
-                                    try(final PreparedStatement statement = connection.prepareStatement(sql)){
+                                    try(final CallableStatement statement = connection.prepareCall(sql)){
 
                                         final Clob requestClob = connection.createClob();
                                         requestClob.setString(1,clobRequestValue);
-                                        statement.setClob(1, requestClob);
+                                        statement.setClob(2, requestClob);
 
                                         statement.execute();
 
@@ -346,9 +345,9 @@ public class JdbcExecutionsIT extends TestNGCitrusTestRunner {
                                 try {
                                     final Connection connection = jdbcDriver.connect(serverUrl, new Properties());
                                     Assert.assertNotNull(connection);
-                                    try(final PreparedStatement statement = connection.prepareStatement(sql)){
+                                    try(final CallableStatement statement = connection.prepareCall(sql)){
 
-                                        statement.setBlob(1, blobRequestValue.getInputStream());
+                                        statement.setBlob(2, blobRequestValue.getInputStream());
                                         statement.execute();
 
                                         final ResultSet resultSet = statement.getResultSet();
