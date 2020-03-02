2df5bbcb1dc92abc1fd47c0e5759b0c8c16f2be6
Christoph Deppisch <cdeppisch@redhat.com>
2020-03-02 10:14:47 +0100

    refactor(citrus-3.0): Move integration tests to citrus-spring-integration modules
    citrus-integration combines integration tests for multiple endpoint modules. Move those tests to individual modules for better maintainability reasons and in order to fail fast in Maven builds.

--- endpoints/citrus-spring-integration/src/test/java/com/consol/citrus/integration/MessageChannelIT.java
+++ endpoints/citrus-spring-integration/src/test/java/com/consol/citrus/integration/MessageChannelIT.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.consol.citrus.channel;
+package com.consol.citrus.integration;
 
 import com.consol.citrus.annotations.CitrusXmlTest;
 import com.consol.citrus.testng.AbstractTestNGCitrusTest;
