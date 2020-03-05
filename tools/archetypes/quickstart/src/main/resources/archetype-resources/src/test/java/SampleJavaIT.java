3efb185e0229a3bac713f6a89323442d88c09550
Christoph Deppisch <cdeppisch@redhat.com>
2020-03-05 16:10:40 +0100

    refactor(citrus-3.0): Fix archetype generated sources

--- tools/archetypes/quickstart/src/main/resources/archetype-resources/src/test/java/SampleJavaIT.java
+++ tools/archetypes/quickstart/src/main/resources/archetype-resources/src/test/java/SampleJavaIT.java
@@ -3,7 +3,9 @@ package ${package};
 import org.testng.annotations.Test;
 
 import com.consol.citrus.annotations.CitrusTest;
-import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
+import com.consol.citrus.testng.TestNGCitrusSupport;
+
+import static com.consol.citrus.actions.EchoAction.Builder.echo;
 
 /**
  * This is a sample Java DSL Citrus integration test.
@@ -11,17 +13,17 @@ import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
  * @author Citrus
  */
 @Test
-public class SampleJavaIT extends TestNGCitrusTestDesigner {
+public class SampleJavaIT extends TestNGCitrusSupport {
 
     @CitrusTest
     public void echoToday() {
         variable("now", "citrus:currentDate()");
 
-        echo("Today is: ${now}");
+        run(echo("Today is: ${now}"));
     }
 
     @CitrusTest(name = "SampleJavaTest.sayHello")
     public void sayHello() {
-        echo("Hello Citrus!");
+        run(echo("Hello Citrus!"));
     }
 }
--- tools/archetypes/soap/src/main/resources/archetype-resources/src/test/java/HelloService_Ok_IT.java
+++ tools/archetypes/soap/src/main/resources/archetype-resources/src/test/java/HelloService_Ok_IT.java
@@ -3,14 +3,14 @@ package ${package};
 import org.testng.annotations.Test;
 
 import com.consol.citrus.annotations.CitrusXmlTest;
-import com.consol.citrus.testng.AbstractTestNGCitrusTest;
+import com.consol.citrus.testng.TestNGCitrusSupport;
 
 /**
  * This is a sample Citrus integration test using SOAP client and server.
  * @author Citrus
  */
 @Test
-public class HelloService_Ok_IT extends AbstractTestNGCitrusTest {
+public class HelloService_Ok_IT extends TestNGCitrusSupport {
 
     @CitrusXmlTest(name = "HelloService_Ok_IT")
     public void helloServiceOk() {}
