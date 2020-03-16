bb22a86efcc8944452beda503cdeb42db6e0c6f5
Christoph Deppisch <cdeppisch@redhat.com>
2020-03-16 15:20:52 +0100

    refactor(citrus-3.0): Also build vintage modules in Travis CI

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "${SONAR_SCANNER_HOME}" != "" ]; then
-    COMMAND="mvn --batch-mode -q clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=citrus"
+    COMMAND="mvn --batch-mode -q clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=citrus -Pvintage"
 else
-    COMMAND="mvn --batch-mode -q clean install"
+    COMMAND="mvn --batch-mode -q clean install -Pvintage"
 fi
 
 echo ${COMMAND}
