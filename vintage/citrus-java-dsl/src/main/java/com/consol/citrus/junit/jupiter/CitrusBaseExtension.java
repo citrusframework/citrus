edc65c5602d223c9820cb0b13fa7b40f138f231a
Christoph Deppisch <cdeppisch@redhat.com>
2021-01-12 09:41:38 +0100

    refactor(citrus-3.0): Move vintage CitrusBaseExtension to citrus-java-dsl module
    Users should be using CitrusSpringSupport instead.

--- vintage/citrus-java-dsl/src/main/java/com/consol/citrus/junit/jupiter/CitrusBaseExtension.java
+++ vintage/citrus-java-dsl/src/main/java/com/consol/citrus/junit/jupiter/CitrusBaseExtension.java
@@ -1,11 +1,14 @@
 /*
- * Copyright 2006-2017 the original author or authors.
+ * Copyright 2021 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -31,7 +34,9 @@ import org.junit.jupiter.api.extension.TestInstancePostProcessor;
  * After suite automatically includes Citrus report generation.
  *
  * @author Christoph Deppisch
+ * @deprecated in favor of using {@link com.consol.citrus.junit.jupiter.spring.CitrusSpringSupport}
  */
+@Deprecated
 public class CitrusBaseExtension implements BeforeAllCallback, TestInstancePostProcessor {
 
     /** Test suite name */
