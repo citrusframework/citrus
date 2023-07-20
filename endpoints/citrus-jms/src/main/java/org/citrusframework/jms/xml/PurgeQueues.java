bc719c87be5e3415b81a7d4e00c0176a5c8c612a
Christoph Deppisch <cdeppisch@redhat.com>
2023-07-20 19:16:19 +0200

    chore: Add JMS XML action DSL support

---
+++ endpoints/citrus-jms/src/main/java/org/citrusframework/jms/xml/PurgeQueues.java
@@ -0,0 +1,191 @@
+/*
+ *  Copyright 2023 the original author or authors.
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements. See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.citrusframework.jms.xml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.jms.ConnectionFactory;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlTransient;
+import jakarta.xml.bind.annotation.XmlType;
+import org.citrusframework.TestAction;
+import org.citrusframework.TestActionBuilder;
+import org.citrusframework.TestActor;
+import org.citrusframework.jms.actions.PurgeJmsQueuesAction;
+import org.citrusframework.spi.ReferenceResolver;
+import org.citrusframework.spi.ReferenceResolverAware;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "description",
+        "queue",
+})
+@XmlRootElement(name = "purge-jms-queues")
+public class PurgeQueues implements TestActionBuilder<TestAction>, ReferenceResolverAware {
+
+    @XmlTransient
+    private final PurgeJmsQueuesAction.Builder builder = new PurgeJmsQueuesAction.Builder();
+
+    @XmlElement
+    private String description;
+    @XmlAttribute
+    private String actor;
+
+    @XmlAttribute
+    protected String connectionFactory;
+
+    @XmlAttribute
+    protected String timeout;
+
+    @XmlAttribute
+    protected String sleep;
+
+    @XmlElement
+    protected List<Queue> queue;
+
+    @XmlTransient
+    private ReferenceResolver referenceResolver;
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setConnectionFactory(String connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    public String getConnectionFactory() {
+        return connectionFactory;
+    }
+
+    public void setTimeout(String timeout) {
+        this.timeout = timeout;
+    }
+
+    public String getTimeout() {
+        return timeout;
+    }
+
+    public void setSleep(String sleep) {
+        this.sleep = sleep;
+    }
+
+    public String getSleep() {
+        return sleep;
+    }
+
+    public void setQueue(List<Queue> queue) {
+        this.queue = queue;
+    }
+
+    public List<Queue> getQueue() {
+        if (queue == null) {
+            queue = new ArrayList<>();
+        }
+
+        return queue;
+    }
+
+    @Override
+    public TestAction build() {
+        builder.setReferenceResolver(referenceResolver);
+        builder.description(description);
+
+        for (Queue queue : getQueue()) {
+            if (queue.name != null) {
+                builder.queue(queue.getName());
+            }
+
+            if (queue.ref != null && referenceResolver != null) {
+                builder.queue(referenceResolver.resolve(queue.ref, jakarta.jms.Queue.class));
+            }
+        }
+
+        if (timeout != null) {
+            builder.timeout(Long.parseLong(timeout));
+        }
+
+        if (sleep != null) {
+            builder.sleep(Long.parseLong(sleep));
+        }
+
+        if (referenceResolver != null) {
+            if (actor != null) {
+                builder.actor(referenceResolver.resolve(actor, TestActor.class));
+            }
+
+            if (connectionFactory != null) {
+                builder.connectionFactory(referenceResolver.resolve(connectionFactory, ConnectionFactory.class));
+            }
+        }
+
+        return builder.build();
+    }
+
+    @Override
+    public void setReferenceResolver(ReferenceResolver referenceResolver) {
+        this.referenceResolver = referenceResolver;
+    }
+
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+            "name"
+    })
+    public static class Queue {
+        @XmlAttribute
+        protected String name;
+
+        @XmlAttribute
+        protected String ref;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setRef(String ref) {
+            this.ref = ref;
+        }
+
+        public String getRef() {
+            return ref;
+        }
+    }
+}
--- endpoints/citrus-jms/src/test/java/org/citrusframework/jms/xml/PurgeQueuesTest.java
+++ endpoints/citrus-jms/src/test/java/org/citrusframework/jms/xml/PurgeQueuesTest.java
@@ -1,4 +1,23 @@
-package org.citrusframework.jms.yaml;
+/*
+ *  Copyright 2023 the original author or authors.
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements. See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.citrusframework.jms.xml;
 
 import java.util.HashMap;
 
@@ -12,8 +31,8 @@ import org.citrusframework.TestCase;
 import org.citrusframework.TestCaseMetaInfo;
 import org.citrusframework.jms.actions.PurgeJmsQueuesAction;
 import org.citrusframework.jms.endpoint.TextMessageImpl;
-import org.citrusframework.yaml.YamlTestLoader;
-import org.citrusframework.yaml.actions.YamlTestActionBuilder;
+import org.citrusframework.xml.XmlTestLoader;
+import org.citrusframework.xml.actions.XmlTestActionBuilder;
 import org.mockito.Mock;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -24,7 +43,10 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 
-public class PurgeQueuesTest extends AbstractYamlActionTest {
+/**
+ * @author Christoph Deppisch
+ */
+public class PurgeQueuesTest extends AbstractXmlActionTest {
 
     @Mock
     private ConnectionFactory connectionFactory;
@@ -41,10 +63,11 @@ public class PurgeQueuesTest extends AbstractYamlActionTest {
 
     @Test
     public void shouldLoadJmsActions() throws Exception {
-        YamlTestLoader testLoader = createTestLoader("classpath:org/citrusframework/jms/yaml/purge-queues-test.yaml");
+        XmlTestLoader testLoader = createTestLoader("classpath:org/citrusframework/jms/xml/purge-queues-test.xml");
 
         context.getReferenceResolver().bind("connectionFactory", connectionFactory);
         context.getReferenceResolver().bind("myConnectionFactory", connectionFactory);
+        context.getReferenceResolver().bind("myQueue", queue);
 
         reset(connectionFactory, connection, session, messageConsumer);
 
@@ -60,7 +83,7 @@ public class PurgeQueuesTest extends AbstractYamlActionTest {
         Assert.assertEquals(result.getName(), "PurgeQueuesTest");
         Assert.assertEquals(result.getMetaInfo().getAuthor(), "Christoph");
         Assert.assertEquals(result.getMetaInfo().getStatus(), TestCaseMetaInfo.Status.FINAL);
-        Assert.assertEquals(result.getActionCount(), 2L);
+        Assert.assertEquals(result.getActionCount(), 3L);
         Assert.assertEquals(result.getTestAction(0).getClass(), PurgeJmsQueuesAction.class);
         Assert.assertEquals(result.getTestAction(0).getName(), "purge-queue");
 
@@ -74,7 +97,7 @@ public class PurgeQueuesTest extends AbstractYamlActionTest {
         Assert.assertEquals(action.getQueueNames().size(), 1);
         Assert.assertEquals(action.getQueueNames().get(0), "JMS.Queue.1");
 
-        action = (PurgeJmsQueuesAction) result.getTestAction(actionIndex);
+        action = (PurgeJmsQueuesAction) result.getTestAction(actionIndex++);
         Assert.assertEquals(action.getReceiveTimeout(), 125L);
         Assert.assertEquals(action.getSleepTime(), 250L);
         Assert.assertNotNull(action.getConnectionFactory());
@@ -82,12 +105,19 @@ public class PurgeQueuesTest extends AbstractYamlActionTest {
         Assert.assertEquals(action.getQueueNames().size(), 2);
         Assert.assertEquals(action.getQueueNames().get(0), "JMS.Queue.2");
         Assert.assertEquals(action.getQueueNames().get(1), "JMS.Queue.3");
+
+        action = (PurgeJmsQueuesAction) result.getTestAction(actionIndex);
+        Assert.assertNotNull(action.getConnectionFactory());
+        Assert.assertEquals(action.getQueues().size(), 1);
+        Assert.assertEquals(action.getQueues().get(0), queue);
+        Assert.assertEquals(action.getQueueNames().size(), 0);
     }
 
     @Test
     public void shouldLookupTestActionBuilder() {
-        Assert.assertTrue(YamlTestActionBuilder.lookup().containsKey("purgeQueues"));
-        Assert.assertTrue(YamlTestActionBuilder.lookup("purgeQueues").isPresent());
-        Assert.assertEquals(YamlTestActionBuilder.lookup("purgeQueues").get().getClass(), PurgeQueues.class);
+        Assert.assertTrue(XmlTestActionBuilder.lookup("purge-jms-queues").isPresent());
+        Assert.assertEquals(XmlTestActionBuilder.lookup("purge-jms-queues").get().getClass(), PurgeQueues.class);
+        Assert.assertTrue(XmlTestActionBuilder.lookup("purgeQueues").isPresent());
+        Assert.assertEquals(XmlTestActionBuilder.lookup("purgeQueues").get().getClass(), PurgeQueues.class);
     }
 }
