900c018f073a6ebd417ed2d3749c73cb4c5b5893
Timon Borter <timon.borter@gmx.ch>
2023-11-03 19:37:51 +0100

    fix(#1046): bugs in resource path resolvers
    * make use of auto-closeable on `InputStream`
    * fix loading from XML properties

--- core/citrus-api/src/main/java/org/citrusframework/message/MessageSelector.java
+++ core/citrus-api/src/main/java/org/citrusframework/message/MessageSelector.java
@@ -29,6 +29,7 @@ public interface MessageSelector {
     /**
      * Resolves all available selectors from resource path lookup. Scans classpath for validator meta information
      * and instantiates those selectors.
+     *
      * @return
      */
     static Map<String, MessageSelectorFactory> lookup() {
@@ -39,14 +40,16 @@ public interface MessageSelector {
                 factories.forEach((k, v) -> logger.debug(String.format("Found message selector '%s' as %s", k, v.getClass())));
             }
         }
+
         return factories;
     }
 
     /**
      * Checks weather this selector should accept given message or not. When accepting the message the
      * selective consumer is provided with the message otherwise the message is skipped for this consumer.
-     * @param message
-     * @return true to accept the message, false to decline.
+     *
+     * @param message the message to check
+     * @return true if the message will be accepted, false otherwise.
      */
     boolean accept(Message message);
 
@@ -54,6 +57,7 @@ public interface MessageSelector {
      * Special message selector accepts all messages on queue.
      */
     final class AllAcceptingMessageSelector implements MessageSelector {
+
         public boolean accept(Message message) {
             return true;
         }
@@ -65,18 +69,21 @@ public interface MessageSelector {
     interface MessageSelectorFactory {
 
         /**
-         * Check if this factories is able to create a message selector for given key.
-         * @param key
-         * @return
+         * Check if this factory is able to create a message selector for given key.
+         *
+         * @param key selector key
+         * @return true if the factory accepts the key, false otherwise.
          */
         boolean supports(String key);
 
         /**
          * Create new message selector for given predicates.
-         * @param key
-         * @param value
-         * @param context
-         * @return
+         *
+         * @param key selector key
+         * @param value selector value
+         * @param context test context
+         *
+         * @return the created selector
          */
         MessageSelector create(String key, String value, TestContext context);
     }
