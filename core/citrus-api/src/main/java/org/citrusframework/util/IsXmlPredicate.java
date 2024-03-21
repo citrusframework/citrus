d1a0da9b306c6e6d04b4fa535b1a43d2bc0da384
Christoph Deppisch <cdeppisch@redhat.com>
2024-03-21 09:19:39 +0100

    chore: Update license headers
    - Add missing license headers
    - Use same license headers

--- core/citrus-api/src/main/java/org/citrusframework/util/IsXmlPredicate.java
+++ core/citrus-api/src/main/java/org/citrusframework/util/IsXmlPredicate.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.citrusframework.util;
 
 import java.util.function.Predicate;
@@ -6,7 +22,7 @@ import java.util.function.Predicate;
  * Tests if a string represents a XML. An empty string is considered to be a valid XML.
  */
 public class IsXmlPredicate implements Predicate<String> {
-    
+
     private static final IsXmlPredicate INSTANCE = new IsXmlPredicate();
 
     private IsXmlPredicate() {
@@ -19,7 +35,7 @@ public class IsXmlPredicate implements Predicate<String> {
 
     @Override
     public boolean test(String toTest) {
-        
+
         if (toTest != null) {
             toTest = toTest.trim();
         }
