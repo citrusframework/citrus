2ce639d4a1a6ce7e0c2f8702019e793de75208ae
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2024-05-29 13:47:42 +0200

    fix: use ISO date format for random JSON property values

--- connectors/citrus-openapi/src/main/java/org/citrusframework/openapi/OpenApiTestDataGenerator.java
+++ connectors/citrus-openapi/src/main/java/org/citrusframework/openapi/OpenApiTestDataGenerator.java
@@ -32,6 +32,7 @@ import org.springframework.util.StringUtils;
  * enforce the specification rules.
  *
  * @author Christoph Deppisch
+ * @author Ralf Ueberfuhr
  */
 public class OpenApiTestDataGenerator {
 
@@ -119,7 +120,7 @@ public class OpenApiTestDataGenerator {
             }
 
             if (schema.format != null && schema.format.equals("date")) {
-                payload.append("citrus:currentDate()");
+                payload.append("citrus:currentDate('yyyy-MM-dd')");
             } else if (schema.format != null && schema.format.equals("date-time")) {
                 payload.append("citrus:currentDate('yyyy-MM-dd'T'hh:mm:ss')");
             } else if (StringUtils.hasText(schema.pattern)) {
