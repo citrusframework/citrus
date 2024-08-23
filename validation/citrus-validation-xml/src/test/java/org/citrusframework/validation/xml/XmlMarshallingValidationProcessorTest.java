33dbeef37bc427914a11170f885a0fcc7e475c5f
Leon Schenk <989485+leonschenk@users.noreply.github.com>
2024-08-23 23:40:53 +0200

    Added a testcase for XmlMarshallingValidationProcessor.Builder

---
+++ validation/citrus-validation-xml/src/test/java/org/citrusframework/validation/xml/XmlMarshallingValidationProcessorTest.java
@@ -0,0 +1,42 @@
+package org.citrusframework.validation.xml;
+
+import org.citrusframework.UnitTestSupport;
+import org.citrusframework.message.DefaultMessage;
+import org.citrusframework.spi.ReferenceResolver;
+import org.citrusframework.spi.SimpleReferenceResolver;
+import org.citrusframework.validation.GenericValidationProcessor;
+import org.citrusframework.xml.Unmarshaller;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class XmlMarshallingValidationProcessorTest extends UnitTestSupport {
+    final Unmarshaller unmarshaller = Object::toString;
+    final GenericValidationProcessor<String> validationProcessor = (payload, headers, context) ->
+            logger.info("Validating message %s".formatted(payload));
+
+    @Test
+    void builderWithoutUnmarshallerTest() {
+        XmlMarshallingValidationProcessor<String> build = XmlMarshallingValidationProcessor.Builder.validate(validationProcessor)
+                .unmarshaller(unmarshaller)
+                .build();
+
+        build.setReferenceResolver(new SimpleReferenceResolver());
+
+        assertDoesNotThrow(() -> build.validate(new DefaultMessage().setPayload("hi"), null));
+    }
+
+    @Test
+    void builderWithUnmarshallerTest() {
+        final ReferenceResolver referenceResolver = new SimpleReferenceResolver();
+        referenceResolver.bind("anyName", unmarshaller);
+
+        XmlMarshallingValidationProcessor<String> build = XmlMarshallingValidationProcessor.Builder.validate(validationProcessor)
+                .withReferenceResolver(referenceResolver)
+                .build();
+
+        build.setReferenceResolver(referenceResolver);
+
+        assertDoesNotThrow(() -> build.validate(new DefaultMessage().setPayload("bye"), null));
+    }
+}
