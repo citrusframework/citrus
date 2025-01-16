2d5ccf1c30cb71a5412343da3e3cf40b506bcc3f
Timon Borter <timon.borter@gmx.ch>
2025-01-16 18:04:24 +0100

    chore: updated formatting

--- validation/citrus-validation-json/src/main/java/org/citrusframework/functions/core/EscapeJsonFunction.java
+++ validation/citrus-validation-json/src/main/java/org/citrusframework/functions/core/EscapeJsonFunction.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.citrusframework.functions.core;
 
 import org.apache.commons.lang3.StringUtils;
@@ -8,6 +24,7 @@ import java.util.List;
 import java.util.Objects;
 
 import static org.apache.commons.lang3.StringEscapeUtils.escapeJson;
+
 /**
  * This function takes a JSON string as input and escapes all double quotes within it.
  *
@@ -16,16 +33,17 @@ import static org.apache.commons.lang3.StringEscapeUtils.escapeJson;
  *
  * <p>Example input: <code>"{\"mySuperJson\": \"valium\"}"</code></p>
  * <p>Example output: <code>"{\\\"mySuperJson\\\": \\\"valium\\\"}"</code></p>
- *
  */
 public class EscapeJsonFunction implements Function {
     @Override
     public String execute(List<String> list, TestContext testContext) {
-        if (Objects.isNull(list) || list.size() != 1 || StringUtils.isEmpty(list.get(0)) || list.get(0).isEmpty()) {
+        if (Objects.isNull(list) || list.size() != 1
+                || StringUtils.isEmpty(list.get(0)) || list.get(0).isEmpty()) {
             throw new IllegalArgumentException(
                     "Parameter List is input is invalid (null, empty, or contains more than one string). The input must be a single non-empty string containing a valid JSON, wrapped in double quotes to be transformed into a valid string."
             );
         }
+
         return escapeJson(list.get(0));
     }
 }
