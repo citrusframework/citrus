73530ee8b8215cb78af5d594e423ab6bedc5813e
Thorsten Schlathoelter <thorsten.schlathoelter@postfinance.ch>
2025-01-30 18:37:12 +0100

    test: disable tests when network is unreachable
    - Added a condition checker to skip tests if the network is unreachable
    - Helps avoid failures when running behind a company proxy
    - support deactivation a container test by skip property

--- runtime/citrus-yaml/src/test/java/org/citrusframework/yaml/container/WaitForTest.java
+++ runtime/citrus-yaml/src/test/java/org/citrusframework/yaml/container/WaitForTest.java
@@ -28,10 +28,14 @@ import org.citrusframework.container.Wait;
 import org.citrusframework.message.DefaultMessage;
 import org.citrusframework.message.DefaultMessageStore;
 import org.citrusframework.message.MessageStore;
+import org.citrusframework.util.TestUtils;
 import org.citrusframework.yaml.YamlTestLoader;
 import org.citrusframework.yaml.actions.AbstractYamlActionTest;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
+
+import static org.citrusframework.util.TestUtils.HTTPS_CITRUSFRAMEWORK_ORG;
 
 public class WaitForTest extends AbstractYamlActionTest {
 
@@ -42,7 +46,10 @@ public class WaitForTest extends AbstractYamlActionTest {
 
     @Test
     public void shouldLoadWaitFor() {
-        String httpUrl = "https://citrusframework.org";
+
+        if (!TestUtils.isNetworkReachable()) {
+            throw new SkipException("Test skipped because citrus is not reachable. We are probably running behind a proxy.");
+        }
         String filePath = "classpath:org/citrusframework/yaml/test-request-payload.xml";
 
         MessageStore messageStore = new DefaultMessageStore();
@@ -70,11 +77,11 @@ public class WaitForTest extends AbstractYamlActionTest {
         validateWaitAction(action, "10000", "2000", condition);
 
         action = (Wait) result.getTestAction(actionIndex++);
-        condition = getHttpCondition(httpUrl, DEFAULT_RESPONSE_CODE, DEFAULT_TIMEOUT);
+        condition = getHttpCondition(HTTPS_CITRUSFRAMEWORK_ORG, DEFAULT_RESPONSE_CODE, DEFAULT_TIMEOUT);
         validateWaitAction(action, DEFAULT_WAIT_TIME, DEFAULT_INTERVAL, condition);
 
         action = (Wait) result.getTestAction(actionIndex++);
-        condition = getHttpCondition(httpUrl + "/doesnotexist", "404", "2000");
+        condition = getHttpCondition(HTTPS_CITRUSFRAMEWORK_ORG + "/doesnotexist", "404", "2000");
         ((HttpCondition)condition).setMethod("GET");
         validateWaitAction(action, "3000", DEFAULT_INTERVAL, condition);
 
