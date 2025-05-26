cf3ce1b4d78d67acfcfe3c072baccbc623faa18d
Christoph Deppisch <cdeppisch@redhat.com>
2025-05-26 10:15:04 +0200

    chore: Fix Citrus JBang dist script
    - Module citrus-validation-yaml has not been released yet

--- tools/jbang/dist/CitrusJBang.java
+++ tools/jbang/dist/CitrusJBang.java
@@ -28,7 +28,6 @@
 //DEPS org.citrusframework:citrus-yaml
 //DEPS org.citrusframework:citrus-http
 //DEPS org.citrusframework:citrus-validation-json
-//DEPS org.citrusframework:citrus-validation-yaml
 //DEPS org.citrusframework:citrus-validation-xml
 package main;
 
