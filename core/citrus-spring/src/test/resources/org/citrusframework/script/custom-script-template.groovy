70d35cec9de661ebc31223103156f8c06e013675
Christoph Deppisch <christoph.deppisch@web.de>
2026-07-22 20:15:45 +0200

    fix(#1054): Fix GroovyAction ScriptExecutor imports

--- core/citrus-spring/src/test/resources/org/citrusframework/script/custom-script-template.groovy
+++ core/citrus-spring/src/test/resources/org/citrusframework/script/custom-script-template.groovy
@@ -1,7 +1,7 @@
 import org.citrusframework.*
 import org.citrusframework.variable.*
 import org.citrusframework.context.TestContext
-import org.citrusframework.script.GroovyAction.ScriptExecutor
+import org.citrusframework.groovy.actions.GroovyAction.ScriptExecutor
 import org.testng.Assert
 
 public class GScript implements ScriptExecutor {
