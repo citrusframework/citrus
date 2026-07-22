a1c2c9f4a0613887df01a195d3e6343718867f16
Christoph Deppisch <christoph.deppisch@web.de>
2026-07-22 22:44:57 +0200

    fix(#1054): Fix split packages for citrus-base endpoint.adapter

---
+++ core/citrus-base/src/test/java/org/citrusframework/base/endpoint/adapter/mapping/HeaderMappingKeyExtractorTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.citrusframework.base.endpoint.adapter.mapping;
+
+import org.citrusframework.endpoint.adapter.mapping.HeaderMappingKeyExtractor;
+import org.citrusframework.exceptions.CitrusRuntimeException;
+import org.citrusframework.message.DefaultMessage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HeaderMappingKeyExtractorTest {
+
+    @Test
+    public void testExtractMappingKey() {
+        HeaderMappingKeyExtractor extractor = new HeaderMappingKeyExtractor();
+        extractor.setHeaderName("Foo");
+
+        Assert.assertEquals(extractor.extractMappingKey(new DefaultMessage("Foo")
+                .setHeader("Foo", "foo")
+                .setHeader("Bar", "bar")), "foo");
+    }
+
+    @Test
+    public void testExtractMappingKeyWithoutHeaderNameSet() {
+        HeaderMappingKeyExtractor extractor = new HeaderMappingKeyExtractor();
+
+        try {
+            extractor.extractMappingKey(new DefaultMessage("Foo")
+                    .setHeader("Foo", "foo")
+                    .setHeader("Bar", "bar"));
+            Assert.fail("Missing exception due to unknown header");
+        } catch (CitrusRuntimeException e) {
+            Assert.assertTrue(e.getMessage().startsWith("Unable to find header"));
+        }
+    }
+
+    @Test
+    public void testExtractMappingKeyWithUnknownHeaderName() {
+        HeaderMappingKeyExtractor extractor = new HeaderMappingKeyExtractor("UNKNOWN");
+
+        try {
+            extractor.extractMappingKey(new DefaultMessage("Foo")
+                    .setHeader("Foo", "foo")
+                    .setHeader("Bar", "bar"));
+            Assert.fail("Missing exception due to unknown header");
+        } catch (CitrusRuntimeException e) {
+            Assert.assertTrue(e.getMessage().startsWith("Unable to find header 'UNKNOWN'"));
+        }
+    }
+}
---
+++ validation/citrus-validation-xml/src/test/java/org/citrusframework/xml/endpoint/adapter/mapping/XPathPayloadMappingKeyExtractorTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.citrusframework.xml.endpoint.adapter.mapping;
+
+import org.citrusframework.exceptions.CitrusRuntimeException;
+import org.citrusframework.message.DefaultMessage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class XPathPayloadMappingKeyExtractorTest {
+
+    @Test
+    public void testExtractMappingKey() {
+        XPathPayloadMappingKeyExtractor extractor = new XPathPayloadMappingKeyExtractor();
+        extractor.setXpathExpression("local-name(//MessageBody/*)");
+
+        Assert.assertEquals(extractor.extractMappingKey(new DefaultMessage(
+                "<MessageBody><Foo>foo</Foo></MessageBody>")), "Foo");
+
+        Assert.assertEquals(extractor.extractMappingKey(new DefaultMessage(
+                "<MessageBody><Bar>bar</Bar></MessageBody>")), "Bar");
+    }
+
+    @Test
+    public void testExtractMappingKeyWithoutXpathExpressionSet() {
+        XPathPayloadMappingKeyExtractor extractor = new XPathPayloadMappingKeyExtractor();
+
+        Assert.assertEquals(extractor.extractMappingKey(new DefaultMessage(
+                "<Foo>foo</Foo>")), "Foo");
+
+        Assert.assertEquals(extractor.extractMappingKey(new DefaultMessage(
+                "<Bar>bar</Bar>")), "Bar");
+    }
+
+    @Test
+    public void testRouteMessageWithBadXpathExpression() {
+        XPathPayloadMappingKeyExtractor extractor = new XPathPayloadMappingKeyExtractor();
+        extractor.setXpathExpression("//I_DO_NOT_EXIST");
+
+        try {
+            extractor.extractMappingKey(new DefaultMessage(
+                    "<MessageBody><Foo>foo</Foo></MessageBody>"));
+            Assert.fail("Missing exception due to bad XPath expression");
+        } catch (CitrusRuntimeException e) {
+            Assert.assertEquals(e.getMessage(), "No result for XPath expression: '//I_DO_NOT_EXIST'");
+        }
+    }
+}
