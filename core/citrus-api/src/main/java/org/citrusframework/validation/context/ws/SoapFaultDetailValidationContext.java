16a92a2cc0f0bf193715e8f22b45b5a4631f3e32
Christoph Deppisch <christoph.deppisch@web.de>
2026-07-23 21:58:00 +0200

    fix(#1054): Fix split packages for citrus-api validation-ws

--- core/citrus-api/src/main/java/org/citrusframework/validation/context/ws/SoapFaultDetailValidationContext.java
+++ core/citrus-api/src/main/java/org/citrusframework/validation/context/ws/SoapFaultDetailValidationContext.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.citrusframework.validation.ws;
+package org.citrusframework.validation.context.ws;
 
 import org.citrusframework.validation.context.xml.XmlMessageValidationContext;
 import org.citrusframework.validation.context.xml.XpathMessageValidationContext;
--- core/citrus-api/src/main/java/org/citrusframework/validation/context/ws/SoapMessageValidationContextBuilder.java
+++ core/citrus-api/src/main/java/org/citrusframework/validation/context/ws/SoapMessageValidationContextBuilder.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.citrusframework.validation.ws;
+package org.citrusframework.validation.context.ws;
 
 public interface SoapMessageValidationContextBuilder {
 
