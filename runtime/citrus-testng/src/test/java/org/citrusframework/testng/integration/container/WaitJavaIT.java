5f4a54dc16cf9bee29ced11b7680da8f3f3bae8b
Christoph Deppisch <christoph.deppisch@web.de>
2026-07-24 13:31:48 +0200

    fix(#1054): Fix split packages for citrus-testng tests

---
+++ runtime/citrus-testng/src/test/java/org/citrusframework/testng/integration/container/WaitJavaIT.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.citrusframework.testng.integration.container;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.concurrent.Executors;
+
+import com.sun.net.httpserver.HttpServer;
+import org.citrusframework.TestActionSupport;
+import org.citrusframework.annotations.CitrusTest;
+import org.citrusframework.testng.integration.common.FileHelper;
+import org.citrusframework.message.MessageType;
+import org.citrusframework.spi.Resources;
+import org.citrusframework.testng.spring.TestNGCitrusSpringSupport;
+import org.citrusframework.util.SocketUtils;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Test
+public class WaitJavaIT extends TestNGCitrusSpringSupport implements TestActionSupport {
+
+    private final int serverPort = SocketUtils.findAvailableTcpPort();
+    private HttpServer server;
+
+    @BeforeClass
+    public void startHttpServer() throws IOException {
+        server = HttpServer.create(new InetSocketAddress("localhost", serverPort), 0);
+        server.createContext("/test", httpExchange -> httpExchange.sendResponseHeaders(200, 0));
+        server.setExecutor(Executors.newSingleThreadExecutor());
+        server.start();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void stopHttpServer() {
+        if (server != null) {
+            server.stop(0);
+        }
+    }
+
+    @CitrusTest
+    public void waitMessage() {
+        //GIVEN
+        String messageName = "myTestMessage";
+
+        run(parallel().actions(
+                sequential().actions(
+                        //WHEN
+                        waitFor()
+                                .message()
+                                .name(messageName)
+                ),
+                sequential().actions(
+                        //THEN
+                        send("direct:waitQueue")
+                                .message()
+                                .name(messageName)
+                                .body("Wait for me")
+                                .header("Operation", "waitForMe"),
+
+                        receive("direct:waitQueue")
+                                .selector(Collections.singletonMap("Operation", "waitForMe"))
+                                .message()
+                                    .type(MessageType.PLAINTEXT)
+                                    .name(messageName)
+                                    .body("Wait for me")
+                                    .header("Operation", "waitForMe")
+                )
+        ));
+    }
+
+    @CitrusTest
+    public void waitFile() {
+        run(waitFor()
+                .file()
+                .resource(Resources.fromClasspath("citrus.properties").file()));
+    }
+
+    @CitrusTest
+    public void waitHttp() {
+        run(waitFor()
+                .http()
+                .url(String.format("http://localhost:%s/test", serverPort)));
+    }
+
+    @CitrusTest
+    public void waitAction() {
+        run(waitFor()
+                .execution()
+                .interval(300L)
+                .milliseconds(500L)
+                .action(sleep().milliseconds(250L)));
+    }
+
+    @CitrusTest
+    public void waitForFileUsingResource() {
+        File file = FileHelper.createTmpFile();
+
+        run(waitFor()
+                .file()
+                .resource(file));
+    }
+
+    @CitrusTest
+    public void waitForFileUsingPath() {
+        File file = FileHelper.createTmpFile();
+
+        run(waitFor()
+                .file()
+                .path(file.toURI().toString()));
+    }
+}
