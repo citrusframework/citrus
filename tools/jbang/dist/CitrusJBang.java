c8ef28b415145dea95d8a5859e45c27ecd6bcd9a
Christoph Deppisch <christoph.deppisch@web.de>
2026-07-29 14:41:49 +0200

    Release 4.10.3

--- tools/jbang/dist/CitrusJBang.java
+++ tools/jbang/dist/CitrusJBang.java
@@ -18,8 +18,8 @@
 
 //JAVA 17+
 //REPOS mavencentral
-//DEPS org.citrusframework:citrus-bom:${citrus.jbang.version:4.10.1}@pom
-//DEPS org.citrusframework:citrus-jbang:${citrus.jbang.version:4.10.1}
+//DEPS org.citrusframework:citrus-bom:${citrus.jbang.version:4.10.3}@pom
+//DEPS org.citrusframework:citrus-jbang:${citrus.jbang.version:4.10.3}
 package main;
 
 import org.citrusframework.jbang.CitrusJBangMain;
